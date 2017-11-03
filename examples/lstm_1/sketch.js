05f261784b22d90d5204cdacf5c6fe3c86c0d724
Daniel Shiffman <daniel@shiffman.net>
2017-11-03 16:09:52 -0400

    comments for LSTM 1 example

--- examples/lstm_1/sketch.js
+++ examples/lstm_1/sketch.js
@@ -9,12 +9,13 @@ Originally ported to ES6 with deeplearn.js by Cristóbal Valenzuela
 ===
 */
 
-var lstm;
+let lstm;
 
 function setup() {
   noCanvas();
 
   // Create the LSTM Generator
+  // Pass in a model directory
   lstm = new p5ml.LSTMGenerator('./../../models/lstm/shakespear/')
 
   // Grab the DOM elements
@@ -22,17 +23,21 @@ function setup() {
   let lengthSlider = select('#lenSlider');
   let tempSlider = select('#tempSlider');
   let button = select('#generate');
+
+  // DOM element events
   button.mousePressed(generate);
   lengthSlider.input(updateSliders);
   tempSlider.input(updateSliders);
 
+  // Update the slider values
   function updateSliders() {
     select('#length').html(lengthSlider.value())
     select('#temperature').html(tempSlider.value())
   }
 
+
+  // Generate new text
   function generate() {
-     // Update the length and temperature span elements
 
     // Grab the original text
     let original = textInput.value();
@@ -40,16 +45,23 @@ function setup() {
     let txt = original.toLowerCase();
 
     // Check if there's something to send
+    // TODO: library should be be able to generate text with
+    // no seed text by seeding with random original text
     if (txt.length > 0) {
-      // Here is the data to post
+
+      // This is what the LSTM generator needs
+      // Seed text, temperature, length to outputs
+      // TODO: What are the defaults?
       let data = {
         seed: txt,
         temperature: tempSlider.value(),
         length: lengthSlider.value()
       }
+
       // Generate text with the lstm
       lstm.generate(data, gotData);
 
+      // When it's done
       function gotData(result) {
         select('#result').html(txt + result.generated);
       }
