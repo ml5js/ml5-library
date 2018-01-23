80627d660b962b582b5d3cfa5d557f408a9773e0
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-01-23 14:22:39 -0500

    add imgs to experiments

--- website/pages/en/experiments.js
+++ website/pages/en/experiments.js
@@ -8,37 +8,49 @@
 const React = require('react');
 
 const CompLibrary = require('../../core/CompLibrary.js');
+const MarkdownBlock = CompLibrary.MarkdownBlock;
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
 const siteConfig = require(process.cwd() + '/siteConfig.js');
 
+
 class Help extends React.Component {
   render() {
+    // image is not working.
     const demos = [
       {
-        content: "Teachable Machines demo",
+        content: "Simple example base on Google's Teachable Machines Project",
+        image: siteConfig.baseUrl + "img/teachable.gif",
         title: 'Teachable Machines',
+        imageLink: "https://github.com/ITPNYU/p5-deeplearn-js/tree/master/demos/teachableMachine",
+        imageAlign: "left"
       },
       {
-        content: 'A web based text editor with RNN/LSTM capabilities',
+        content: 'An experimental web text editor that runs a LSTM model while you write to suggest new lines',
+        image: siteConfig.baseUrl + "img/selected_stories.gif",
         title: 'Selected Stories',
+        imageLink: "https://cvalenzuela.github.io/Selected_Stories/",
+        imageAlign: "left"
       },
       {
         content: 'Machine Learning Pong Game in The Browser',
+        image: siteConfig.baseUrl + "img/pongml.jpg",
         title: 'Pong ML',
+        imageLink: "https://github.com/matamalaortiz/Pong-ML",
+        imageAlign: "left"
       }
     ];
 
     return (
       <div className="docMainWrapper wrapper">
         <Container className="mainContainer documentContainer postContainer">
-          <div className="post">
+          <div className="post" id="experiments">
             <header className="postHeader">
               <h2>Experiments</h2>
             </header>
-            <p>A collection of experiments and demos built with p5ML.js</p>
-            <GridBlock contents={demos} layout="threeColumn" />
+            <p>A collection of experiments and demos built with p5ML.js.</p>
+            <GridBlock contents={demos} layout="threeColumn"/>
           </div>
         </Container>
       </div>
