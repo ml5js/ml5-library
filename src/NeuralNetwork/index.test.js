9c7430af0e54b22c021881d4ad825ed243ca5b8b
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-03-10 13:12:16 -0500

    feat(NN and YOLO): removing wip classes and moving them to a wip branch
    Removing YOLO and Neural Network class since they are not ready for use yet. YOLO needs some updates
    from dl and Neural Network needs to be updated to be more efficient (it was just a toy example). I'm
    effectively removing them from this series of commit and moving it to a WIP branch

--- src/NeuralNetwork/index.test.js
+++
@@ -1,4 +0,0 @@
-// Just a test
-test('true should be true', () => {
-  expect(true).toBe(true);
-});
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import * as dl from 'deeplearn';
 import ImageNet from './ImageNet/index';
 import KNNImageClassifier from './KNNImage/index';
 import LSTMGenerator from './Lstm/index';
-import NeuralNetwork from './NeuralNetwork/index';
 import Word2Vec from './Word2vec/index';
 import FastStyle from './FastStyle/index';
-import Yolo from './Yolo/index';
 import * as imageUtils from './utils/imageUtilities';
 
 window.dl = dl;
@@ -13,9 +11,7 @@ module.exports = {
   ImageNet,
   KNNImageClassifier,
   LSTMGenerator,
-  NeuralNetwork,
   FastStyle,
-  Yolo,
   Word2Vec,
   ...imageUtils,
 };
