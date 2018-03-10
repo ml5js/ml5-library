ae65e0fec0d4d3f0f58d02a540b2b846e97b76e9
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-03-09 20:41:45 -0500

    refactor(Word2vec): update to match dl 0.5
    Updated Word2Vec to match dl 0.5

--- src/Word2vec/index.js
+++ src/Word2vec/index.js
@@ -1,16 +1,14 @@
 /*
-Word2Vec implementation
-Based on https://github.com/shiffman/p5-word2vec
+Word2Vec
 */
 
-import { ENV, Array1D, Scalar, util } from 'deeplearn';
+import { div, tensor, add, sub, util } from 'deeplearn';
 
 class Word2Vec {
   constructor(vectors, callback) {
     this.ready = false;
     this.model = {};
     this.modelSize = 0;
-    this.math = ENV.math;
     this.loadVectors(vectors, callback);
   }
 
@@ -19,7 +17,7 @@ class Word2Vec {
       .then(response => response.json())
       .then((json) => {
         Object.keys(json.vectors).forEach((word) => {
-          this.model[word] = Array1D.new(json.vectors[word]);
+          this.model[word] = tensor(json.vectors[word]);
         });
         this.modelSize = Object.keys(json).length;
         callback();
@@ -40,7 +38,7 @@ class Word2Vec {
 
   average(inputs, max = 1) {
     const sum = Word2Vec.addOrSubtract(this.model, inputs, 'ADD');
-    const avg = this.math.arrayDividedByScalar(sum, Scalar.new(inputs.length));
+    const avg = div(sum, tensor(inputs.length));
     return Word2Vec.nearest(this.model, avg, inputs.length, inputs.length + max);
   }
 
@@ -75,19 +73,17 @@ class Word2Vec {
     if (notFound.length > 0) {
       throw new Error(`Invalid input, vector not found for: ${notFound.toString()}`);
     }
-    return ENV.math.scope(() => {
-      let result = vectors[0];
-      if (operation === 'ADD') {
-        for (let i = 1; i < vectors.length; i += 1) {
-          result = ENV.math.add(result, vectors[i]);
-        }
-      } else {
-        for (let i = 1; i < vectors.length; i += 1) {
-          result = ENV.math.subtract(result, vectors[i]);
-        }
+    let result = vectors[0];
+    if (operation === 'ADD') {
+      for (let i = 1; i < vectors.length; i += 1) {
+        result = add(result, vectors[i]);
       }
-      return result;
-    });
+    } else {
+      for (let i = 1; i < vectors.length; i += 1) {
+        result = sub(result, vectors[i]);
+      }
+    }
+    return result;
   }
 
   static nearest(model, input, start, max) {
