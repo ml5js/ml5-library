a16465716b90db1b609397a064186ef5725fcd40
handav <hannahdavis0220@gmail.com>
2018-04-26 18:44:54 -0400

    adding Crepe pitch detection model

--- experiments/musicvae/index.js
+++
@@ -1,3 +0,0 @@
-console.log('hey');
-
-//var pitch = new ml5.MusicVae(0, 100);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 // Copyright (c) 2018 ml5
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-// import * as dl from 'deeplearn';
-// import ImageClassifier from './ImageClassifier/index';
-// import KNNImageClassifier from './KNNImageClassifier/index';
-// import LSTMGenerator from './LSTM/index';
+import * as dl from 'deeplearn';
+import ImageClassifier from './ImageClassifier/index';
+import KNNImageClassifier from './KNNImageClassifier/index';
+import LSTMGenerator from './LSTM/index';
 import Crepe from './Crepe/index';
-// import Word2Vec from './Word2vec/index';
-// import StyleTransfer from './StyleTransfer/index';
-// import * as imageUtils from './utils/imageUtilities';
+import Word2Vec from './Word2vec/index';
+import StyleTransfer from './StyleTransfer/index';
+import * as imageUtils from './utils/imageUtilities';
 module.exports = {
-  // ImageClassifier,
-  // KNNImageClassifier,
-  // LSTMGenerator,
+  ImageClassifier,
+  KNNImageClassifier,
+  LSTMGenerator,
   Crepe,
-  // StyleTransfer,
-  // Word2Vec,
-  // ...imageUtils,
-  // dl,
+  StyleTransfer,
+  Word2Vec,
+  ...imageUtils,
+  dl,
 };
