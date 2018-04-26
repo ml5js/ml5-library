fa83f86bb80996586dda778dedb0887267b74740
handav <hannahdavis0220@gmail.com>
2018-04-26 17:35:50 -0400

    trying to simplify

--- experiments/Crepe/index.js
+++ experiments/Crepe/index.js
@@ -1,53 +1,46 @@
-console.log('in crepe');
-
-
-var capture;
 var crepe;
 var ellipseSize;
-var pastEllipseSize = 1;
+let voiceLow = 100;
+let voiceHigh = 500;
 
 function setup() {
-  console.log('in setup');
-  createCanvas(390, 240);
-  crepe = new ml5.Crepe(getAudioContext());
-  //console.log(getAudioContext());
-  // capture = createCapture(AUDIO, function(){
-  //     console.log(capture);
-  //     var crepe = new ml5.Crepe(capture);
-  // });
-  //capture.size(320, 240);
+  createCanvas(400, 300);
+  noLoop();
+  const videoElement = createCapture(AUDIO, stream => {
+    console.log(AUDIO);
+    videoElement.volume(0);
+    console.log(videoElement);
+    console.log('stream created');
+    crepe = new ml5.Crepe(getAudioContext(), stream); 
+    loop(); 
+  });
+  noStroke();
 }
 
 function parse(result){
     var splitResult = result.split(" Hz");
-    console.log(float(splitResult[0]));
     return float(splitResult[0])
 }
 
 function draw() {
+  if (!crepe){
+    console.log("Crepe not yet initialized");
+    return;
+  }
   var results = crepe.getResults();
   if (results){
-      console.log("past ", pastEllipseSize);
     if (results['result'] == "no voice"){
-        newEllipseSize = 1;
-        console.log("new ", newEllipseSize);
-        ellipseSize = lerp(pastEllipseSize, newEllipseSize, 0.1);
-        console.log("ellipse size: ", ellipseSize);
+      newEllipseSize = 1;
+      ellipseSize = lerp(ellipseSize, newEllipseSize, 0.1);
     }else{
-        newEllipseSize = map(parse(results['result']), 0, 1200, 0, 300);
-        console.log("new ", newEllipseSize);
-        ellipseSize = lerp(pastEllipseSize, newEllipseSize, 0.1);
-        console.log("ellipse size: ", ellipseSize);
+      newEllipseSize = map(parse(results['result']), voiceLow, voiceHigh, 0, 500);
+      ellipseSize = lerp(ellipseSize, newEllipseSize, 0.1);
     }
   }else{
     ellipseSize = 1;
   }
-  console.log('\n');
   background(0);
   fill(255);
-  noStroke();
-
-  ellipse(200, 150, ellipseSize, ellipseSize);
-  pastEllipseSize = ellipseSize;
+  ellipse(width/2, height/2, ellipseSize, ellipseSize);
 }
 
--- src/Crepe/index.js
+++ src/Crepe/index.js
@@ -4,29 +4,26 @@ https://github.com/marl/crepe/tree/gh-pages
 https://marl.github.io/crepe/crepe.js
 */
 
-// can replace audio processing with
-// https://p5js.org/reference/#/p5/createCapture
-
 import * as tf from '@tensorflow/tfjs';
 
 class Crepe {
   // in here are the functions to make exposed
-  constructor(audioContext) {
+  constructor(audioContext, stream) {
     this.audioContext = audioContext;
+    this.stream = stream;
     this.initTF();
   }
 
   async initTF() {
     try {
       console.log('Loading Keras model...');
-      window.model = await tf.loadModel('model/model.json');
+      this.model = await tf.loadModel('model/model.json');
       console.log('Model loading complete');
     } catch (e) {
       console.error(e);
     }
     this.initAudio();
   }
-
 
   // perform resampling the audio to 16000 Hz, on which the model is trained.
   // setting a sample rate in AudioContext is not supported by most browsers at the moment.
@@ -63,7 +60,7 @@ class Crepe {
         const framestd = tf.tensor(tf.norm(zeromean).dataSync()/Math.sqrt(1024));
         const normalized = tf.div(zeromean, framestd);
         const input = normalized.reshape([1, 1024]);
-        const activation = model.predict([input]).reshape([360]);
+        const activation = this.model.predict([input]).reshape([360]);
 
         // the confidence of voicing activity and the argmax bin
         const confidence = activation.max().dataSync()[0];
@@ -83,12 +80,11 @@ class Crepe {
         const predictedCent = productSum / weightSum;
         const predictedHz = 10 * Math.pow(2, predictedCent / 1200.0);
 
-        // update the UI and the activation plot
+        // update
         let result = (confidence > 0.5) ? predictedHz.toFixed(3) + ' Hz' : 'no voice';
         const strlen = result.length;
         for (let i = 0; i < 11 - strlen; i++) result = result;
         this.results.result = result;
-        // this.updateActivation(activation.dataSync());
       });
     });
   }
@@ -97,54 +93,49 @@ class Crepe {
     return this.results;
   }
 
+  processStream(stream) {
+    console.log('Setting up AudioContext ...');
+    console.log('Audio context sample rate = ' + this.audioContext.sampleRate);
+    const mic = this.audioContext.createMediaStreamSource(stream);
+
+    // We need the buffer size that is a power of two
+    // and is longer than 1024 samples when resampled to 16000 Hz.
+    // In most platforms where the sample rate is 44.1 kHz or 48 kHz,
+    // this will be 4096, giving 10-12 updates/sec.
+    const minBufferSize = this.audioContext.sampleRate / 16000 * 1024;
+    for (var bufferSize = 4; bufferSize < minBufferSize; bufferSize *= 2);
+    console.log('Buffer size = ' + bufferSize);
+    const scriptNode = this.audioContext.createScriptProcessor(bufferSize, 1, 1);
+    scriptNode.onaudioprocess = this.processMicrophoneBuffer.bind(this);
+    // It seems necessary to connect the stream to a sink
+    // for the pipeline to work, contrary to documentataions.
+    // As a workaround, here we create a gain node with zero gain,
+    // and connect temp to the system audio output.
+    const gain = this.audioContext.createGain();
+    gain.gain.setValueAtTime(0, this.audioContext.currentTime);
+
+    mic.connect(scriptNode);
+    scriptNode.connect(gain);
+    gain.connect(this.audioContext.destination);
+
+    if (this.audioContext.state === 'running') {
+      console.log('Running ...');
+    } else {
+      console.error('User gesture needed to start AudioContext, please click');
+      // user gesture (like click) is required to start AudioContext, in some browser versions
+      // status('<a href="javascript:crepe.resume();" style="color:red;">*
+      // Click here to start the demo *</a>')
+    }
+  }
+
   initAudio() {
-    // if (!navigator.getUserMedia) {
-    //   console.log('no navigator get user media');
-    //   if (navigator.mediaDevices) {
-    //     navigator.getUserMedia = navigator.mediaDevices.getUserMedia;
-    //   } else {
-    //     navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    //   }
-    // }
     if (this.audioContext) {
       console.log('Initializing audio');
-      navigator.getUserMedia({ audio: true }, (stream) => {
-        console.log('Setting up AudioContext ...');
-        console.log(this.audioContext);
-        console.log('Audio context sample rate = ' + this.audioContext.sampleRate);
-        const mic = this.audioContext.createMediaStreamSource(stream);
-
-        // We need the buffer size that is a power of two
-        // and is longer than 1024 samples when resampled to 16000 Hz.
-        // In most platforms where the sample rate is 44.1 kHz or 48 kHz,
-        // this will be 4096, giving 10-12 updates/sec.
-        const minBufferSize = this.audioContext.sampleRate / 16000 * 1024;
-        for (var bufferSize = 4; bufferSize < minBufferSize; bufferSize *= 2);
-        console.log('Buffer size = ' + bufferSize);
-        const scriptNode = this.audioContext.createScriptProcessor(bufferSize, 1, 1);
-        scriptNode.onaudioprocess = this.processMicrophoneBuffer.bind(this);
-        // It seems necessary to connect the stream to a sink
-        // for the pipeline to work, contrary to documentataions.
-        // As a workaround, here we create a gain node with zero gain,
-        // and connect temp to the system audio output.
-        const gain = this.audioContext.createGain();
-        gain.gain.setValueAtTime(0, this.audioContext.currentTime);
-
-        mic.connect(scriptNode);
-        scriptNode.connect(gain);
-        gain.connect(this.audioContext.destination);
-
-        if (this.audioContext.state === 'running') {
-          console.log('Running ...');
-        } else {
-          console.error('User gesture needed to start AudioContext, please click');
-          // user gesture (like click) is required to start AudioContext, in some browser versions
-          // status('<a href="javascript:crepe.resume();" style="color:red;">*
-          // Click here to start the demo *</a>')
-        }
-      }, (message) => {
-        console.error('Error: Could not access microphone - ', message);
-      });
+      try { 
+        this.processStream(this.stream);
+      } catch(e) {
+        console.error('Error: Could not access microphone - ', e);
+      }
     } else {
       console.error('Could not access microphone - getUserMedia not available');
     }
