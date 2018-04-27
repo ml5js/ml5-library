4635b383f1fb2627603b7d5fed60f635c556e7d8
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-04-27 15:35:43 -0400

    Ported KNN to Transfer learning, the next step will be to make it part of the ImageClassifier

--- src/TransferLearning/index.js
+++ src/TransferLearning/index.js
@@ -21,7 +21,7 @@ const DEFAULTS = {
 };
 
 class TransferLearning {
-  constructor(video, callback = () => {}, options = {}) {
+  constructor(video, options = {}, callback = () => {}) {
     this.ready = false;
     this.modelPath = 'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
     this.hasAnyTrainedClass = false;
@@ -47,14 +47,24 @@ class TransferLearning {
     const mobilenet = await tf.loadModel(this.modelPath);
     const layer = mobilenet.getLayer('conv_pw_13_relu');
 
-    // tf.tidy(() => mobilenet.predict(webcam.capture())); // Warm up
+    if (this.video) {
+      tf.tidy(() => mobilenet.predict(TransferLearning.imgToTensor(this.video))); // Warm up
+    }
+
     return tf.model({ inputs: mobilenet.inputs, outputs: layer.output });
   }
 
-  addImage(input, label, callback = () => {}) {
+  addImage(label, callback = () => {}, input = null) {
     if (this.ready) {
       tf.tidy(() => {
-        const processedImg = TransferLearning.imgToTensor(input);
+        let processedImg;
+
+        if (input) {
+          processedImg = TransferLearning.imgToTensor(input);
+        } else {
+          processedImg = TransferLearning.imgToTensor(this.video);
+        }
+
         const prediction = this.mobilenet.predict(processedImg);
 
         const y = tf.tidy(() => tf.oneHot(tf.tensor1d([label]), this.numClasses));
@@ -84,6 +94,7 @@ class TransferLearning {
     }
 
     this.isPredicting = false;
+
     this.model = tf.sequential({
       layers: [
         tf.layers.flatten({ inputShape: [7, 7, 256] }),
@@ -121,7 +132,7 @@ class TransferLearning {
     });
   }
 
-  async predict(input, callback) {
+  async predict(input, callback = () => {}) {
     this.isPredicting = true;
 
     const predictedClass = tf.tidy(() => {
@@ -136,78 +147,6 @@ class TransferLearning {
 
     await tf.nextFrame();
   }
-
-  // async addImageFromVideo(index, callback) {
-  //   if (this.ready && this.video) {
-  //     const image = dl.fromPixels(this.video);
-  //     this.knn.addImage(image, index);
-  //     this.hasAnyTrainedClass = true;
-  //     if (callback) {
-  //       callback();
-  //     }
-  //   }
-  // }
-
-  // async predictFromImage(input, callback) {
-  //   if (this.ready && this.hasAnyTrainedClass) {
-  //     const image = dl.fromPixels(input);
-  //     const results = await this.knn.predictClass(image);
-  //     callback(results);
-  //   }
-  // }
-
-  // async predictFromVideo(callback) {
-  //   if (this.ready && this.hasAnyTrainedClass && this.video) {
-  //     const image = dl.fromPixels(this.video);
-  //     const results = await this.knn.predictClass(image);
-  //     callback(results);
-  //   }
-  // }
-
-  // getClassExampleCount() {
-  //   if (this.ready) {
-  //     return this.knn.getClassExampleCount();
-  //   }
-  //   return null;
-  // }
-
-  // clearClass(classIndex, callback) {
-  //   if (this.ready && this.hasAnyTrainedClass) {
-  //     this.knn.clearClass(classIndex);
-  //     if (callback) {
-  //       callback();
-  //     }
-  //   }
-  // }
-
-  // save(name) {
-  //   const logits = this.knn.classLogitsMatrices;
-  //   const tensors = logits.map((t) => {
-  //     if (t) {
-  //       return t.dataSync();
-  //     }
-  //     return null;
-  //   });
-  //   const fileName = name || Date.now();
-  //   io.saveFile(`${fileName}.json`, JSON.stringify({ logits, tensors }));
-  // }
-
-  // load(path, callback) {
-  //   io.loadFile(path, (data) => {
-  //     const tensors = data.tensors.map((tensor, i) => {
-  //       if (tensor) {
-  //         const values = Object.keys(tensor).map(v => tensor[v]);
-  //         return dl.tensor(values, data.logits[i].shape, data.logits[i].dtype);
-  //       }
-  //       return null;
-  //     });
-  //     this.hasAnyTrainedClass = true;
-  //     this.knn.setClassLogitsMatrices(tensors);
-  //     if (callback) {
-  //       callback();
-  //     }
-  //   });
-  // }
 
   static cropImage(img) {
     const size = Math.min(img.shape[0], img.shape[1]);
