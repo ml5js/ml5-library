8c0ce65df999582003ec22e3e1daf49a50079780
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-05-21 17:59:47 -0400

    update to posenet

--- src/PoseNet/index.js
+++ src/PoseNet/index.js
@@ -15,19 +15,22 @@ const DEFAULTS = {
   imageScaleFactor: 0.5,
   outputStride: 16,
   flipHorizontal: false,
+  minConfidence: 0.5,
+  maxPoseDetections: 5,
+  scoreThreshold: 0.5,
+  nmsRadius: 20,
 };
 
 class PoseNet extends ImageAndVideo {
-  constructor(video, optionsOrCallback, cb = () => {}) {
-    super(video, 315);
+  constructor(videoOrCallback, optionsOrCallback, cb = null) {
+    super(videoOrCallback, 315);
     let options = {};
     let callback = cb;
-    this.isPredicting = false;
-    this.minConfidence = 0.5;
 
-    if (typeof optionsOrCallback === 'object') {
+    if (typeof videoOrCallback === 'function') {
+      callback = videoOrCallback;
+    } else if (typeof optionsOrCallback === 'object') {
       options = optionsOrCallback;
-      callback = cb;
     } else if (typeof optionsOrCallback === 'function') {
       callback = optionsOrCallback;
     }
@@ -35,17 +38,21 @@ class PoseNet extends ImageAndVideo {
     this.imageScaleFactor = options.imageScaleFactor || DEFAULTS.imageScaleFactor;
     this.outputStride = options.outputStride || DEFAULTS.outputStride;
     this.flipHorizontal = options.flipHorizontal || DEFAULTS.flipHorizontal;
+    this.minConfidence = options.minConfidence || DEFAULTS.minConfidence;
 
+    // TODO: Specify model
     posenet.load()
       .then((net) => {
         this.net = net;
-        this.singlePose(callback);
+        if (this.video && callback) {
+          this.singlePose(callback);
+        }
       })
       .catch((err) => { console.error(`Error loading the model: ${err}`); });
   }
 
-  skeleton(keypoints) {
-    return posenet.getAdjacentKeyPoints(keypoints, this.minConfidence);
+  skeleton(keypoints, confidence = this.minConfidence) {
+    return posenet.getAdjacentKeyPoints(keypoints, confidence);
   }
 
   /* eslint max-len: ["error", { "code": 180 }] */
@@ -68,11 +75,24 @@ class PoseNet extends ImageAndVideo {
       });
   }
 
-  // posenet.getAdjacentKeyPoints(keypoints, confidence)
+  multiPose(inputOrCallback, cb = () => {}) {
+    let input;
+    let callback;
 
-  // multiPose() {
+    if (inputOrCallback instanceof HTMLImageElement || inputOrCallback instanceof HTMLVideoElement) {
+      input = inputOrCallback;
+      callback = cb;
+    } else if (typeof inputOrCallback === 'function' && this.video) {
+      input = this.video;
+      callback = inputOrCallback;
+    }
 
-  // }
+    this.net.estimateMultiplePoses(input, this.imageScaleFactor, this.flipHorizontal, this.outputStride)
+      .then((pose) => {
+        callback(pose);
+        tf.nextFrame().then(() => { this.multiPose(callback); });
+      });
+  }
 }
 
 export default PoseNet;
