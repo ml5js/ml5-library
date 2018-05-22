8e50be42f8107c3ec8ec9af6aa84c0aff8c2ac8d
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-05-22 16:52:34 -0400

    Reversing StyleTransfer to work with custom loadCheckpoints. There's a refactor of this method that works with a keras implementation of style transfer but needs support for UpSampling2D layer. See https://github.com/tensorflow/tfjs/issues/250

--- src/StyleTransfer/index.js
+++ src/StyleTransfer/index.js
@@ -6,102 +6,100 @@
 /* eslint max-len: "off" */
 /*
 Fast Style Transfer
-Heavily based out of: https://github.com/zaidalyafeai/Fast-Style-Transfer-Keras-TF.js
 */
 
 import * as tf from '@tensorflow/tfjs';
-import { processVideo } from '../utils/imageUtilities';
+import CheckpointLoader from '../utils/checkpointLoader';
+import { array3DToImage } from '../utils/imageUtilities';
 
 class StyleTransfer {
-  constructor(model, videoOrCallback, cb) {
+  constructor(model, callback) {
     this.ready = false;
-    this.modelPath = model;
-    this.modelReady = false;
-    this.model = null;
-    this.imageSize = 256;
-
-    let callback;
-    if (videoOrCallback instanceof HTMLVideoElement) {
-      callback = cb;
-      this.video = processVideo(videoOrCallback, this.imageSize, () => {
-        this.videoReady = true;
-      });
-    } else {
-      callback = videoOrCallback;
-      this.video = null;
-    }
-    this.loadModel(callback);
-  }
-
-  async loadModel(callback) {
-    this.model = await tf.loadModel(`${this.modelPath}/model.json`);
-    this.modelReady = true;
-    callback();
-  }
-
-  transfer(inputOrCallback, cb) {
-    let input;
-    let callback;
-
-    if (inputOrCallback instanceof HTMLVideoElement || inputOrCallback instanceof HTMLImageElement) {
-      input = inputOrCallback;
-      callback = cb;
-    } else {
-      input = this.video;
-      callback = inputOrCallback;
-    }
-
-    if (this.modelReady) {
-      const processedImage = StyleTransfer.preprocess(input);
-      const prediction = this.model.predict(processedImage);
-      const output = StyleTransfer.deprocess(prediction);
-      const result = StyleTransfer.tensorToImage(output);
+    this.variableDictionary = {};
+    this.timesScalar = tf.scalar(150);
+    this.plusScalar = tf.scalar(255.0 / 2);
+    this.epsilonScalar = tf.scalar(1e-3);
+    this.loadCheckpoints(model).then(() => {
+      this.ready = true;
       if (callback) {
-        callback(result);
+        callback();
       }
-    }
-
-    console.error('The model has not finished loading');
-    return false;
-  }
-
-  static preprocess(input) {
-    return tf.tidy(() => {
-      const pixels = tf.fromPixels(input).toFloat();
-      const offset = tf.scalar(127.5);
-      const normalized = pixels.sub(offset).div(offset);
-      const batched = normalized.expandDims(0);
-      return batched;
     });
   }
-
-  static deprocess(input) {
-    return tf.tidy(() => {
-      const offset = tf.scalar(127.5);
-      const denormalized = input.mul(offset).add(offset).toInt();
-      const reduced = denormalized.squeeze();
-      return reduced;
-    });
+  async loadCheckpoints(path) {
+    const checkpointLoader = new CheckpointLoader(path);
+    this.variables = await checkpointLoader.getAllVariables();
   }
 
-  static tensorToImage(tensor) {
-    const [height, width] = tensor.shape;
-    const buffer = new Uint8ClampedArray(width * height * 4);
-    const imageData = new ImageData(width, height);
-    const data = tensor.dataSync();
-    let cnt = 0;
-    for (let y = 0; y < height; y += 1) {
-      for (let x = 0; x < width; x += 1) {
-        const pos = ((y * width) + x) * 4;
-        buffer[pos] = data[cnt];
-        buffer[pos + 1] = data[cnt + 1];
-        buffer[pos + 2] = data[cnt + 2];
-        buffer[pos + 3] = 255;
-        cnt += 3;
-      }
+  instanceNorm(input, id) {
+    const [height, width, inDepth] = input.shape;
+    const moments = tf.moments(input, [0, 1]);
+    const mu = moments.mean;
+    const sigmaSq = moments.variance;
+    const shift = this.variables[StyleTransfer.getVariableName(id)];
+    const scale = this.variables[StyleTransfer.getVariableName(id + 1)];
+    const epsilon = this.epsilonScalar;
+    const normalized = tf.div(tf.sub(input.asType('float32'), mu), tf.sqrt(tf.add(sigmaSq, epsilon)));
+    const shifted = tf.add(tf.mul(scale, normalized), shift);
+    return shifted.as3D(height, width, inDepth);
+  }
+
+  convLayer(input, strides, relu, id) {
+    const y = tf.conv2d(input, this.variables[StyleTransfer.getVariableName(id)], [strides, strides], 'same');
+    const y2 = this.instanceNorm(y, id + 1);
+    if (relu) {
+      return tf.relu(y2);
     }
-    imageData.data.set(buffer);
-    return imageData;
+    return y2;
+  }
+
+  residualBlock(input, id) {
+    const conv1 = this.convLayer(input, 1, true, id);
+    const conv2 = this.convLayer(conv1, 1, false, id + 3);
+    return tf.add(conv2, input);
+  }
+
+  convTransposeLayer(input, numFilters, strides, id) {
+    const [height, width] = input.shape;
+    const newRows = height * strides;
+    const newCols = width * strides;
+    const newShape = [newRows, newCols, numFilters];
+    const y = tf.conv2dTranspose(input, this.variables[StyleTransfer.getVariableName(id)], newShape, [strides, strides], 'same');
+    const y2 = this.instanceNorm(y, id + 1);
+    const y3 = tf.relu(y2);
+    return y3;
+  }
+
+  transfer(input) {
+    const image = tf.fromPixels(input);
+    const result = tf.tidy(() => {
+      const conv1 = this.convLayer(image, 1, true, 0);
+      const conv2 = this.convLayer(conv1, 2, true, 3);
+      const conv3 = this.convLayer(conv2, 2, true, 6);
+      const res1 = this.residualBlock(conv3, 9);
+      const res2 = this.residualBlock(res1, 15);
+      const res3 = this.residualBlock(res2, 21);
+      const res4 = this.residualBlock(res3, 27);
+      const res5 = this.residualBlock(res4, 33);
+      const convT1 = this.convTransposeLayer(res5, 64, 2, 39);
+      const convT2 = this.convTransposeLayer(convT1, 32, 2, 42);
+      const convT3 = this.convLayer(convT2, 1, false, 45);
+      const outTanh = tf.tanh(convT3);
+      const scaled = tf.mul(this.timesScalar, outTanh);
+      const shifted = tf.add(this.plusScalar, scaled);
+      const clamped = tf.clipByValue(shifted, 0, 255);
+      const normalized = tf.div(clamped, tf.scalar(255.0));
+      return normalized;
+    });
+    return array3DToImage(result);
+  }
+
+  // Static Methods
+  static getVariableName(id) {
+    if (id === 0) {
+      return 'Variable';
+    }
+    return `Variable_${id}`;
   }
 }
 
