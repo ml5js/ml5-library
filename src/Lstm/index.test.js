33f44ed73b3fd92276dd04c0d510aaf08d7d8099
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-06-01 14:57:15 -0400

    clean up

--- src/Lstm/index.test.js
+++ src/Lstm/index.test.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-=======
 // Copyright (c) 2018 ml5
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 // Just a test
-test('true should be true', () => {
-  expect(true).toBe(true);
-});
->>>>>>> b41f29d749d5a9fb1311630ae9ebde3b8ebd5684
+// test('true should be true', () => {
+//   expect(true).toBe(true);
+// });
