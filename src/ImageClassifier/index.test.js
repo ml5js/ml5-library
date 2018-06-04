f6943443d3faa63019713857aec1843c9d4f5439
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-06-04 11:07:37 -0500

    add Chrome PATH

--- src/ImageClassifier/index.test.js
+++ src/ImageClassifier/index.test.js
@@ -46,32 +46,3 @@ describe('Create an image classifier', () => {
   //   };
   // });
 });
-
-
-// describe('SqueezeNet Classifier', () => {
-//   let SqueezeNetClassifier;
-//   beforeAll((done) => {
-//     SqueezeNetClassifier = new ImageClassifier('SqueezeNet');
-//     done();
-//   });
-
-//   // TODO: Check that the right net is created:
-//   // toEqual compares tf.tensors and since they have different ids they don't match
-//   it('creates a new instance', (done) => {
-//     expect(SqueezeNetClassifier).toEqual(jasmine.objectContaining({
-//       model: 'SqueezeNet',
-//       readyPromise: null,
-//       video: null,
-//     }));
-//     done();
-//   });
-
-// it('makes a prediction', (done) => {
-//   const img = document.createElement('img');
-//   img.src = 'https://ml5js.org/docs/assets/img/bird.jpg';
-//   SqueezeNetClassifier.predict(img, 10, (results) => {
-//     expect(results[0].probability).toEqual('robin, American robin, Turdus migratorius');
-//     done();
-//   });
-// });
-// });
