27ec7d932fae4736d6a422749383c40ae055bded
Dan Oved <stangogh@gmail.com>
2018-06-05 18:16:40 -0400

    Enabling source maps and fine tuned webpack config.
    * Created dev and prod webpack config, by following the conventions in
    https://webpack.js.org/guides/production/
    * For dev config, using eval source map.
    * For prod config, uglifying and doing a standard sourcemap.
    * Since we have a ml5.min.js and sourcemap, there is no longer a near for
    ml5.js
    * Changed the watch task to be yarn start
    
    updated to posenet latest version, which has an es6 dist to enable better viewing of the source code
    
    updated contributing to reflect new build process

---
+++ webpack.dev.babel.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2018 ml5
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { join } from 'path';
+import merge from 'webpack-merge';
+import common from './webpack.common.babel';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    watchContentBase: true,
+    contentBase: join(__dirname, './dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ml5'
+    })
+  ]
+})
---
+++ webpack.prod.babel.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2018 ml5
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import merge from 'webpack-merge';
+import common from './webpack.common.babel';
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
+
+export default merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    filename: 'ml5.min.js'
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
+  ]
+})
