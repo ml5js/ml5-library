9cd0cd3c3cec774db0ba8ca8a1a3e2784cfc3300
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-06-10 01:58:12 -0400

    fix cideo in posenet

--- src/FeatureExtractor/index.js
+++ src/FeatureExtractor/index.js
@@ -7,46 +7,47 @@
 A General Feature Extractor class
 */
 
-const DEFAULTS = {
-  mobilenet: {
-    version: 1,
-    alpha: 1.0,
-    topk: 3,
-    learningRate: 0.0001,
-    hiddenUnits: 100,
-    epochs: 20,
-    numClasses: 2,
-    batchSize: 0.4,
-  },
-};
+// const DEFAULTS = {
+//   mobilenet: {
+//     version: 1,
+//     alpha: 1.0,
+//     topk: 3,
+//     learningRate: 0.0001,
+//     hiddenUnits: 100,
+//     epochs: 20,
+//     numClasses: 2,
+//     batchSize: 0.4,
+//   },
+// };
 
 class FeatureExtractor {
-  constructor(model, inputOrOptionsOrCallback, optionsOrCallback = {}, cb = null) {
-    if (typeof model === 'string') {
-      this.modelName = model;
-    } else {
-      console.error('Please specify a model to use. E.g: "Mobilenet"');
-    }
+  // constructor(model, inputOrOptionsOrCallback, optionsOrCallback = {}, cb = null) {
+  //   if (typeof model === 'string') {
+  //     this.modelName = model;
+  //   } else {
+  //     console.error('Please specify a model to use. E.g: "Mobilenet"');
+  //   }
 
-    this.options = {};
-    let callback = cb;
-    // TODO: handle sound inputs
-    if (inputOrOptionsOrCallback instanceof HTMLVideoElement) {
-      this.input = inputOrOptionsOrCallback;
-    } else if (typeof inputOrOptionsOrCallback === 'object' && inputOrOptionsOrCallback.elt instanceof HTMLVideoElement) {
-      this.input = inputOrOptionsOrCallback.elt; // Handle p5.js video
-    } else if (inputOrOptionsOrCallback === 'object') {
-      this.options = inputOrOptionsOrCallback;
-    } else if (typeof inputOrOptionsOrCallback === 'function') {
-      callback = inputOrOptionsOrCallback;
-    }
+  //   this.options = {};
+  //   let callback = cb;
+  //   // TODO: handle sound inputs
+  //   if (inputOrOptionsOrCallback instanceof HTMLVideoElement) {
+  //     this.input = inputOrOptionsOrCallback;
+  //   } else if (typeof inputOrOptionsOrCallback === 'object'
+  // && inputOrOptionsOrCallback.elt instanceof HTMLVideoElement) {
+  //     this.input = inputOrOptionsOrCallback.elt; // Handle p5.js video
+  //   } else if (inputOrOptionsOrCallback === 'object') {
+  //     this.options = inputOrOptionsOrCallback;
+  //   } else if (typeof inputOrOptionsOrCallback === 'function') {
+  //     callback = inputOrOptionsOrCallback;
+  //   }
 
-    if (optionsOrCallback === 'object') {
-      this.options = optionsOrCallback;
-    } else if (typeof optionsOrCallback === 'function') {
-      callback = optionsOrCallback;
-    }
-  }
+  //   if (optionsOrCallback === 'object') {
+  //     this.options = optionsOrCallback;
+  //   } else if (typeof optionsOrCallback === 'function') {
+  //     callback = optionsOrCallback;
+  //   }
+  // }
 }
 
 export default FeatureExtractor;
