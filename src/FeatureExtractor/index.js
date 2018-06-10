e92411fd1136bfe3d852ad6959b02cd50bddd160
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-06-10 15:41:23 -0400

    Create Image regressor and classifier with mobilenet

--- src/FeatureExtractor/index.js
+++ src/FeatureExtractor/index.js
@@ -4,50 +4,34 @@
 // https://opensource.org/licenses/MIT
 
 /*
-A General Feature Extractor class
+General Feature Extractor Manager
 */
 
-// const DEFAULTS = {
-//   mobilenet: {
-//     version: 1,
-//     alpha: 1.0,
-//     topk: 3,
-//     learningRate: 0.0001,
-//     hiddenUnits: 100,
-//     epochs: 20,
-//     numClasses: 2,
-//     batchSize: 0.4,
-//   },
-// };
+import Mobilenet from './Mobilenet';
 
-class FeatureExtractor {
-  // constructor(model, inputOrOptionsOrCallback, optionsOrCallback = {}, cb = null) {
-  //   if (typeof model === 'string') {
-  //     this.modelName = model;
-  //   } else {
-  //     console.error('Please specify a model to use. E.g: "Mobilenet"');
-  //   }
+/* eslint max-len: ["error", { "code": 180 }] */
+const FeatureExtractor = (model, optionsOrCallback, cb = () => {}) => {
+  let modelName;
+  if (typeof model !== 'string') {
+    console.error('Please specify a model to use. E.g: "Mobilenet"');
+  } else {
+    modelName = model.toLowerCase();
+  }
 
-  //   this.options = {};
-  //   let callback = cb;
-  //   // TODO: handle sound inputs
-  //   if (inputOrOptionsOrCallback instanceof HTMLVideoElement) {
-  //     this.input = inputOrOptionsOrCallback;
-  //   } else if (typeof inputOrOptionsOrCallback === 'object'
-  // && inputOrOptionsOrCallback.elt instanceof HTMLVideoElement) {
-  //     this.input = inputOrOptionsOrCallback.elt; // Handle p5.js video
-  //   } else if (inputOrOptionsOrCallback === 'object') {
-  //     this.options = inputOrOptionsOrCallback;
-  //   } else if (typeof inputOrOptionsOrCallback === 'function') {
-  //     callback = inputOrOptionsOrCallback;
-  //   }
+  let options = {};
+  let callback = cb;
 
-  //   if (optionsOrCallback === 'object') {
-  //     this.options = optionsOrCallback;
-  //   } else if (typeof optionsOrCallback === 'function') {
-  //     callback = optionsOrCallback;
-  //   }
-  // }
-}
+  if (optionsOrCallback === 'object') {
+    options = optionsOrCallback;
+  } else if (typeof optionsOrCallback === 'function') {
+    callback = optionsOrCallback;
+  }
+
+  if (modelName === 'mobilenet') {
+    return new Mobilenet(options, callback);
+  }
+  console.error(`${modelName} is not a valid model.`);
+  return null;
+};
 
 export default FeatureExtractor;
