a745c14819eb1ba3e88dd369d12051695bde2e9c
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-06-10 00:22:55 -0400

    check for model as string

--- src/ImageClassifier/index.js
+++ src/ImageClassifier/index.js
@@ -7,10 +7,11 @@
 Image classifier class
 */
 
+import * as tf from '@tensorflow/tfjs';
 import * as mobilenet from '@tensorflow-models/mobilenet';
-import Video from './../utils/Video';
+// import Video from './../utils/Video';
 
-const IMAGESIZE = 224;
+// const IMAGESIZE = 224;
 const DEFAULTS = {
   mobilenet: {
     version: 1,
@@ -19,13 +20,17 @@ const DEFAULTS = {
   },
 };
 
-class ImageClassifier extends Video {
+class ImageClassifier {
   constructor(modelName, videoOrOptionsOrCallback, optionsOrCallback, cb = null) {
-    super(videoOrOptionsOrCallback, IMAGESIZE);
+    // super(videoOrOptionsOrCallback, IMAGESIZE);
 
     let options = {};
     let callback = cb;
-    if (videoOrOptionsOrCallback === 'object') {
+    if (videoOrOptionsOrCallback instanceof HTMLVideoElement) {
+      this.video = videoOrOptionsOrCallback;
+    } else if (typeof videoOrOptionsOrCallback === 'object' && videoOrOptionsOrCallback.elt instanceof HTMLVideoElement) {
+      this.video = videoOrOptionsOrCallback.elt; // Handle a p5.js video element
+    } else if (videoOrOptionsOrCallback === 'object') {
       options = videoOrOptionsOrCallback;
     } else if (videoOrOptionsOrCallback === 'function') {
       callback = videoOrOptionsOrCallback;
@@ -37,35 +42,28 @@ class ImageClassifier extends Video {
       callback = optionsOrCallback;
     }
 
-    this.modelName = modelName.toLowerCase();
-    this.version = options.version || DEFAULTS[this.modelName].version;
-    this.alpha = options.alpha || DEFAULTS[this.modelName].alpha;
-    this.topk = options.topk || DEFAULTS[this.modelName].topk;
-    this.modelLoaded = false;
-    this.model = null;
-    if (this.modelName === 'mobilenet') {
-      this.modelToUse = mobilenet;
+    if (typeof modelName === 'string') {
+      this.modelName = modelName.toLowerCase();
+      this.version = options.version || DEFAULTS[this.modelName].version;
+      this.alpha = options.alpha || DEFAULTS[this.modelName].alpha;
+      this.topk = options.topk || DEFAULTS[this.modelName].topk;
+      this.modelLoaded = false;
+      this.model = null;
+      if (this.modelName === 'mobilenet') {
+        this.modelToUse = mobilenet;
+      } else {
+        this.modelToUse = null;
+      }
+
+      // Load the model and video if necessary
+      this.modelLoaded = this.loadModel(callback);
     } else {
-      this.modelToUse = null;
       console.error('Please specify a model to use. E.g: "Mobilenet"');
     }
-
-    // Load the model and video if necessary
-    this.modelLoaded = this.loadModel(callback);
   }
 
   async loadModel(callback) {
-    if (this.videoElt) {
-      return this.loadVideo().then(() => {
-        this.modelToUse.load().then((model) => {
-          this.model = model;
-          if (callback) {
-            callback();
-          }
-        });
-      });
-    }
-    return this.modelToUse.load().then((model) => {
+    return this.modelToUse.load(this.version, this.alpha).then((model) => {
       this.model = model;
       if (callback) {
         callback();
@@ -75,27 +73,26 @@ class ImageClassifier extends Video {
 
   async predict(inputNumOrCallback, numOrCallback = null, cb = null) {
     let imgToPredict;
-    let numberOfClasses;
+    let numberOfClasses = this.topk;
     let callback;
 
     // Handle the image to predict
     if (typeof inputNumOrCallback === 'function') {
       imgToPredict = this.video;
       callback = inputNumOrCallback;
+    } else if (typeof inputNumOrCallback === 'number') {
+      imgToPredict = this.video;
+      numberOfClasses = inputNumOrCallback;
     } else if (inputNumOrCallback instanceof HTMLImageElement) {
       imgToPredict = inputNumOrCallback;
     } else if (typeof inputNumOrCallback === 'object' && inputNumOrCallback.elt instanceof HTMLImageElement) {
       imgToPredict = inputNumOrCallback.elt; // Handle p5.js image
-    } else if (typeof numOrCallback === 'number') {
-      imgToPredict = this.video;
-      numberOfClasses = inputNumOrCallback;
     }
 
-    // Handle the callback
-    if (typeof numOrCallback === 'function') {
+    if (typeof numOrCallback === 'number') {
+      numberOfClasses = inputNumOrCallback;
+    } else if (typeof numOrCallback === 'function') {
       callback = numOrCallback;
-    } else if (typeof numOrCallback === 'number') {
-      numberOfClasses = numOrCallback;
     }
 
     if (typeof cb === 'function') {
@@ -104,8 +101,21 @@ class ImageClassifier extends Video {
 
     // Wait for the model to be ready
     await this.modelLoaded;
+    await tf.nextFrame();
 
     // Classify the image using the selected model
+    if (this.video) {
+      if (this.video.readyState >= 2) {
+        return this.model.classify(imgToPredict, numberOfClasses).then((predictions) => {
+          if (callback) {
+            callback(predictions);
+          }
+        });
+      }
+      if (callback) {
+        callback([]);
+      }
+    }
     return this.model.classify(imgToPredict, numberOfClasses).then((predictions) => {
       if (callback) {
         callback(predictions);
