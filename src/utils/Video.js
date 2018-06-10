7440c0223b6a63d6827f5ac594a420ea1729069b
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2018-06-10 02:41:09 -0400

    update YOLO to load video via promise

--- src/utils/Video.js
+++ src/utils/Video.js
@@ -4,33 +4,39 @@
 // https://opensource.org/licenses/MIT
 
 /*
-Base Video class that creates
-a separate video element with a correct
-video size to use with a model
+Image and Video base class
 */
 
 class Video {
   constructor(video, size) {
     this.videoElt = null;
     this.size = size;
+    this.videoReady = false;
 
     if (video instanceof HTMLVideoElement) {
       this.videoElt = video;
     } else if (typeof video === 'object' && video.elt instanceof HTMLVideoElement) {
-      this.videoElt = video.elt; // Handle a p5.js video element
+      // Handle p5.js video element
+      this.videoElt = video.elt;
     }
   }
 
   loadVideo() {
     return new Promise((resolve) => {
       this.video = document.createElement('video');
-      this.video.srcObject = this.videoElt.captureStream();
+      const stream = this.videoElt.captureStream();
+      this.video.srcObject = stream;
       this.video.width = this.size;
       this.video.height = this.size;
       this.video.autoplay = true;
       this.video.playsinline = true;
       this.video.muted = true;
-      resolve();
+      const playPromise = this.video.play();
+      if (playPromise !== undefined) {
+        playPromise.then(() => {
+          resolve();
+        });
+      }
     });
   }
 }
