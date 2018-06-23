4cd6d25ea23a38d144486167942f909de3a7043f
nsthorat <nsthorat@google.com>
2018-06-23 14:56:14 -0400

    set up browserstack

--- src/StyleTransfer/index.test.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2018 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-// Just a test
-// test('true should be true', () => {
-//   expect(true).toBe(true);
-// });
