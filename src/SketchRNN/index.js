943d6eccc5053e52d2a7bc93e59c384efc967dc8
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2018-08-08 01:09:09 +0900

    stylefixes

--- src/SketchRNN/index.js
+++ src/SketchRNN/index.js
@@ -3,8 +3,8 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-/* eslint prefer-destructuring: ["error", {AssignmentExpression: {array: false}}] */
-/* eslint no-await-in-loop: "off" */
+/* eslint prefer-destructuring: ['error', {AssignmentExpression: {array: false}}] */
+/* eslint no-await-in-loop: 'off' */
 /*
 SketchRNN
 */
@@ -13,43 +13,42 @@ import * as ms from '@magenta/sketch';
 import callCallback from '../utils/callcallback';
 import modelPaths from './models';
 
-const pathStartLarge = "https://storage.googleapis.com/quickdraw-models/sketchRNN/large_models/";
-const pathStartSmall = "https://storage.googleapis.com/quickdraw-models/sketchRNN/models/";
-const pathEnd = ".gen.json";
+const pathStartLarge = 'https://storage.googleapis.com/quickdraw-models/sketchRNN/large_models/';
+const pathStartSmall = 'https://storage.googleapis.com/quickdraw-models/sketchRNN/models/';
+const pathEnd = '.gen.json';
 
-class SketchRNN {
-    constructor(checkpointUrl, callback, large=true) {
-        if (modelPaths.has(checkpointUrl)) {
-            if (large) {
-                checkpointUrl = pathStartLarge + checkpointUrl + pathEnd;
-            } else {
-                checkpointUrl = pathStartSmall + checkpointUrl + pathEnd;
-            }
-        }
-        this.defaults = {
-            temperature: 0.65
-        }
-        this.model = new ms.SketchRNN(checkpointUrl);
-        this.penState = this.model.zeroInput();
-        this.rnnState = this.model.zeroState();
-        this.ready = callCallback(this.model.initialize(), callback);
+export default class SketchRNN {
+  constructor(model, callback, large = true) {
+    let checkpointUrl = model;
+    if (modelPaths.has(checkpointUrl)) {
+      checkpointUrl = (large ? pathStartLarge : pathStartSmall) + checkpointUrl + pathEnd;
+    }
+    this.defaults = {
+      temperature: 0.65,
+    };
+    this.model = new ms.SketchRNN(checkpointUrl);
+    this.penState = this.model.zeroInput();
+    this.ready = callCallback(this.model.initialize(), callback);
+  }
+
+  async generateInternal(options, strokes) {
+    const temperature = +options.temperature || this.defaults.temperature;
+
+    await this.ready;
+    if (!this.rnnState) {
+      this.rnnState = this.model.zeroState();
     }
 
-    async generateInternal(options, strokes) {
-        const temperature = +options.temperature || this.defaults.temperature;
-        
-        await this.ready;
-
-        if (strokes !== undefined) {
-            this.model.updateStrokes(strokes, this.rnnState);
-        }
-        this.rnnState = this.model.update(this.penState, this.rnnState);
-        let pdf = this.model.getPDF(this.rnnState, temperature);
-        this.penState = this.model.sample(pdf);
-        return this.penState;
+    if (!strokes) {
+      this.model.updateStrokes(strokes, this.rnnState);
     }
+    this.rnnState = this.model.update(this.penState, this.rnnState);
+    const pdf = this.model.getPDF(this.rnnState, temperature);
+    this.penState = this.model.sample(pdf);
+    return this.penState;
+  }
 
-    async generate(options, strokes, callback) {
-        return callCallback(this.generateInternal(options, strokes), callback);
-    }
+  async generate(options, strokes, callback) {
+    return callCallback(this.generateInternal(options, strokes), callback);
+  }
 }
