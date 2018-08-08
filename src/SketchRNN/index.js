8e64e6f82703fe4e41062b7f2e65bd238e1e7fa0
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2018-08-09 02:47:50 +0900

    some more bugfixes

--- src/SketchRNN/index.js
+++ src/SketchRNN/index.js
@@ -13,18 +13,19 @@ import * as ms from '@magenta/sketch';
 import callCallback from '../utils/callcallback';
 import modelPaths from './models';
 
-const pathStartLarge = 'https://storage.googleapis.com/quickdraw-models/sketchRNN/large_models/';
-const pathStartSmall = 'https://storage.googleapis.com/quickdraw-models/sketchRNN/models/';
-const pathEnd = '.gen.json';
+const PATH_START_LARGE = 'https://storage.googleapis.com/quickdraw-models/sketchRNN/large_models/';
+const PATH_START_SMALL = 'https://storage.googleapis.com/quickdraw-models/sketchRNN/models/';
+const PATH_END = '.gen.json';
 
 export default class SketchRNN {
   constructor(model, callback, large = true) {
     let checkpointUrl = model;
     if (modelPaths.has(checkpointUrl)) {
-      checkpointUrl = (large ? pathStartLarge : pathStartSmall) + checkpointUrl + pathEnd;
+      checkpointUrl = (large ? PATH_START_LARGE : PATH_START_SMALL) + checkpointUrl + PATH_END;
     }
     this.defaults = {
       temperature: 0.65,
+      pixelFactor: 3.0,
     };
     this.model = new ms.SketchRNN(checkpointUrl);
     this.penState = this.model.zeroInput();
@@ -33,14 +34,16 @@ export default class SketchRNN {
 
   async generateInternal(options, strokes) {
     const temperature = +options.temperature || this.defaults.temperature;
+    const pixelFactor = +options.pixelFactor || this.defaults.pixelFactor;
 
     await this.ready;
     if (!this.rnnState) {
       this.rnnState = this.model.zeroState();
+      this.model.setPixelFactor(pixelFactor);
     }
 
-    if (!strokes) {
-      this.model.updateStrokes(strokes, this.rnnState);
+    if (Array.isArray(strokes) && strokes.length) {
+      this.rnnState = this.model.updateStrokes(strokes, this.rnnState);
     }
     this.rnnState = this.model.update(this.penState, this.rnnState);
     const pdf = this.model.getPDF(this.rnnState, temperature);
@@ -50,5 +53,12 @@ export default class SketchRNN {
 
   async generate(options, strokes, callback) {
     return callCallback(this.generateInternal(options, strokes), callback);
+  }
+
+  reset() {
+    this.penState = this.model.zeroInput();
+    if (this.rnnState) {
+      this.rnnState = this.model.zeroState();
+    }
   }
 }
