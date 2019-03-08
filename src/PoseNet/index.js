88c81fe34eb66b3f6afc8003e0c63372f4d31994
Joey Lee <joeyklee@users.noreply.github.com>
2019-03-08 17:55:02 -0500

    remove trailing space causing travis to fail

--- src/PoseNet/index.js
+++ src/PoseNet/index.js
@@ -88,7 +88,6 @@ class PoseNet extends EventEmitter {
     const pose = await this.net.estimateSinglePose(input, this.imageScaleFactor, this.flipHorizontal, this.outputStride);
     const poseWithParts = this.mapParts(pose);
     const result = [{ poseWithParts, skeleton: this.skeleton(pose.keypoints) }];
-    
     this.emit('pose', result);
 
     if (this.video) {
