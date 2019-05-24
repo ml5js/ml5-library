f16d289050ef476c70716982a58d9555fe19989e
Joey Lee <joeyklee@users.noreply.github.com>
2019-05-24 11:03:17 -0400

    Adds jsdoc inline-documentation - work in progress (#378)
    * added jsdoc documentation for imageClassifier
    
    * adds dcgan documentation - needs checking
    
    * Add jsdoc (#382)
    
    * Add jsdocs for CharRNN
    
    * Add jsdocs for CVAE
    
    * Add jsdocs for FeatureExtractor
    
    * Add jsdocs for KNN
    
    * Add jsdocs for PitchDetection
    
    * Add jsdocs for Pix2pix
    
    * Add jsdocs for posenet
    
    * Add jsdocs for Sentiment
    
    * Add jsdocs for styletransfer
    
    * add linebreaks to long lines
    
    * added basic docs to sketchRnn
    
    * added basic docs to unet
    
    * added basic docs to word2vec
    
    * added basic yolo docs

--- src/FeatureExtractor/index.js
+++ src/FeatureExtractor/index.js
@@ -10,6 +10,27 @@ General Feature Extractor Manager
 import Mobilenet from './Mobilenet';
 
 /* eslint max-len: ["error", { "code": 180 }] */
+
+
+/**
+ * @typedef {Object} options
+ * @property {number} version - default 1
+ * @property {number} alpha - default 1.0
+ * @property {number} topk - default 3
+ * @property {number} learningRate - default 0.0001
+ * @property {number} hiddenUnits - default 100
+ * @property {number} epochs - default 20
+ * @property {number} numClasses - default 2
+ * @property {number} batchSize - default 0.4
+ */
+
+
+/**
+ * Create a featureExtractor.
+ * @param {model} model - The model from which extract the learned features. Case-insensitive
+ * @param {options || function} optionsOrCallback - Optional.
+ * @param {function} cb - Optional. 
+ */
 const featureExtractor = (model, optionsOrCallback, cb) => {
   let modelName;
   if (typeof model !== 'string') {
