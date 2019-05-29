ed6edd7c955b32a06c14398de01071a064a88ae3
joeyklee <jsphknglee@gmail.com>
2019-05-29 14:28:56 -0400

    adds part segmentation

--- src/BodyPix/index.js
+++ src/BodyPix/index.js
@@ -43,6 +43,93 @@ class BodyPix {
         return this;
     }
 
+    async segmentWithPartsInternal(imgToSegment){
+        // estimatePartSegmentation
+        await this.ready;
+        await tf.nextFrame();
+
+        if (this.video && this.video.readyState === 0) {
+            await new Promise(resolve => {
+                this.video.onloadeddata = () => resolve();
+            });
+        }
+
+        const rainbow = [
+            [110, 64, 170], [106, 72, 183], [100, 81, 196], [92, 91, 206],
+            [84, 101, 214], [75, 113, 221], [66, 125, 224], [56, 138, 226],
+            [48, 150, 224], [40, 163, 220], [33, 176, 214], [29, 188, 205],
+            [26, 199, 194], [26, 210, 182], [28, 219, 169], [33, 227, 155],
+            [41, 234, 141], [51, 240, 128], [64, 243, 116], [79, 246, 105],
+            [96, 247, 97],  [115, 246, 91], [134, 245, 88], [155, 243, 88]
+          ];
+          
+
+        const segmentation = await this.model.estimatePartSegmentation(imgToSegment)
+        
+        // // wrap up the final js result object
+        const result = {};
+        result.image = bp.toColoredPartImageData(segmentation, rainbow)
+        // result.maskPerson = bp.toColoredPartImageData(segmentation, rainbow)
+
+        if (p5Utils.checkP5()) {
+            const blob1 = await p5Utils.rawToBlob(result.image.data, segmentation.width, segmentation.height);
+            // const blob2 = await p5Utils.rawToBlob(result.maskPerson.data, segmentation.width, segmentation.height);
+            const p5Image1 = await p5Utils.blobToP5Image(blob1);
+            // const p5Image2 = await p5Utils.blobToP5Image(blob2);
+
+            result.image = p5Image1;
+            // result.maskPerson = p5Image2;
+        }
+
+        console.log(result)
+
+        return result;
+
+    }
+
+    async segmentWithParts(optionsOrCallback, cb){
+        let imgToSegment = this.video;
+        let callback;
+
+        // Handle the image to predict
+        if (typeof optionsOrCallback === 'function') {
+            imgToSegment = this.video;
+            callback = optionsOrCallback;
+            // clean the following conditional statement up!
+        } else if (optionsOrCallback instanceof HTMLImageElement) {
+            imgToSegment = optionsOrCallback;
+        } else if (
+            typeof optionsOrCallback === 'object' &&
+            optionsOrCallback.elt instanceof HTMLImageElement
+        ) {
+            imgToSegment = optionsOrCallback.elt; // Handle p5.js image
+        } else if (optionsOrCallback instanceof HTMLCanvasElement) {
+            imgToSegment = optionsOrCallback;
+        } else if (
+            typeof optionsOrCallback === 'object' &&
+            optionsOrCallback.elt instanceof HTMLCanvasElement
+        ) {
+            imgToSegment = optionsOrCallback.elt; // Handle p5.js image
+        } else if (
+            typeof optionsOrCallback === 'object' &&
+            optionsOrCallback.canvas instanceof HTMLCanvasElement
+        ) {
+            imgToSegment = optionsOrCallback.canvas; // Handle p5.js image
+        } else if (!(this.video instanceof HTMLVideoElement)) {
+            // Handle unsupported input
+            throw new Error(
+                'No input image provided. If you want to classify a video, pass the video element in the constructor. ',
+            );
+        }
+
+        if (typeof cb === 'function') {
+            callback = cb;
+        }
+
+        return callCallback(this.segmentWithPartsInternal(imgToSegment), callback);
+
+    }
+
     async segmentInternal(imgToSegment) {
         await this.ready;
         await tf.nextFrame();
@@ -55,19 +142,11 @@ class BodyPix {
 
         const segmentation = await this.model.estimatePersonSegmentation(imgToSegment)
 
-    
-        // // get the p5.Image object
-        // let p5Image;
-        // if (p5Utils.checkP5()) {
-        //     p5Image = await p5Utils.blobToP5Image(blob);
-        // }
-
         // // wrap up the final js result object
         const result = {};
         result.maskBackground = bp.toMaskImageData(segmentation, true);
         result.maskPerson = bp.toMaskImageData(segmentation, false);
 
-        
 
         if (p5Utils.checkP5()) {
             const blob1 = await p5Utils.rawToBlob(result.maskBackground.data, segmentation.width, segmentation.height);
