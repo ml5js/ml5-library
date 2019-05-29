7b26751b634521bab6ddb2df6846a70fabe9e6aa
joeyklee <jsphknglee@gmail.com>
2019-05-29 11:06:00 -0400

    added segment and segmentInternal

--- src/BodyPix/index.js
+++ src/BodyPix/index.js
@@ -10,7 +10,7 @@ BodyPix
 */
 
 
-// import * as tf from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs';
 import * as bp from '@tensorflow-models/body-pix';
 import callCallback from '../utils/callcallback';
 // import Video from '../utils/Video';
@@ -36,10 +36,71 @@ class BodyPix {
         this.ready = callCallback(this.loadModel(), callback);
     }
 
-    async loadModel(){
+    async loadModel() {
         this.model = await bp.load(this.config.multiplier);
+        this.modelReady = true;
         return this;
     }
+
+    async segmentInternal(imgToSegment){
+        await this.ready;
+        await tf.nextFrame();
+
+        if (this.video && this.video.readyState === 0) {
+            await new Promise(resolve => {
+              this.video.onloadeddata = () => resolve();
+            });
+          }
+        
+          return this.model
+          .estimatePersonSegmentation(imgToSegment)
+          .then(result => result);
+
+    }
+
+    async segment(optionsOrCallback, cb) {
+        let imgToSegment = this.video;
+        let callback;
+
+        // Handle the image to predict
+        if (typeof optionsOrCallback === 'function') {
+            imgToSegment = this.video;
+            callback = optionsOrCallback;
+            // clean the following conditional statement up!
+        } else if (optionsOrCallback instanceof HTMLImageElement) {
+            imgToSegment = optionsOrCallback;
+        } else if (
+            typeof optionsOrCallback === 'object' &&
+            optionsOrCallback.elt instanceof HTMLImageElement
+        ) {
+            imgToSegment = optionsOrCallback.elt; // Handle p5.js image
+        } else if (optionsOrCallback instanceof HTMLCanvasElement) {
+            imgToSegment = optionsOrCallback;
+        } else if (
+            typeof optionsOrCallback === 'object' &&
+            optionsOrCallback.elt instanceof HTMLCanvasElement
+        ) {
+            imgToSegment = optionsOrCallback.elt; // Handle p5.js image
+        } else if (
+            typeof optionsOrCallback === 'object' &&
+            optionsOrCallback.canvas instanceof HTMLCanvasElement
+        ) {
+            imgToSegment = optionsOrCallback.canvas; // Handle p5.js image
+        } else if (!(this.video instanceof HTMLVideoElement)) {
+            // Handle unsupported input
+            throw new Error(
+                'No input image provided. If you want to classify a video, pass the video element in the constructor. ',
+            );
+        }
+
+        if (typeof cb === 'function') {
+            callback = cb;
+        }
+
+        return callCallback(this.segmentInternal(imgToSegment), callback);
+    }
+
+
 }
 
 const bodyPix = (videoOrOptionsOrCallback, optionsOrCallback, cb) => {
