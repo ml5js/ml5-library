2417c33d5933faa40220ccd6efe549d2d7825af6
joeyklee <jsphknglee@gmail.com>
2019-05-31 17:48:50 -0400

    changed palette spec to object

--- src/BodyPix/index.js
+++ src/BodyPix/index.js
@@ -20,32 +20,108 @@ const DEFAULTS = {
     "multiplier": 0.75,
     "outputStride": 16,
     "segmentationThreshold": 0.5,
-    "palette": [
-        [110, 64, 170],
-        [106, 72, 183],
-        [100, 81, 196],
-        [92, 91, 206],
-        [84, 101, 214],
-        [75, 113, 221],
-        [66, 125, 224],
-        [56, 138, 226],
-        [48, 150, 224],
-        [40, 163, 220],
-        [33, 176, 214],
-        [29, 188, 205],
-        [26, 199, 194],
-        [26, 210, 182],
-        [28, 219, 169],
-        [33, 227, 155],
-        [41, 234, 141],
-        [51, 240, 128],
-        [64, 243, 116],
-        [79, 246, 105],
-        [96, 247, 97],
-        [115, 246, 91],
-        [134, 245, 88],
-        [155, 243, 88]
-    ]
+    "palette": {
+        // "none": {
+        //     "id": -1,
+        //     "color": [0, 0, 0]
+        // },
+        "leftFace": {
+            "id": 0,
+            "color": [110, 64, 170]
+        },
+        "rightFace": {
+            "id": 1,
+            "color": [106, 72, 183]
+        },
+        "rightUpperLegFront": {
+            "id": 2,
+            "color": [100, 81, 196]
+        },
+        "rightLowerLegBack": {
+            "id": 3,
+            "color": [92, 91, 206]
+        },
+        "rightUpperLegBack": {
+            "id": 4,
+            "color": [84, 101, 214]
+        },
+        "leftLowerLegFront": {
+            "id": 5,
+            "color": [75, 113, 221]
+        },
+        "leftUpperLegFront": {
+            "id": 6,
+            "color": [66, 125, 224]
+        },
+        "leftUpperLegBack": {
+            "id": 7,
+            "color": [56, 138, 226]
+        },
+        "leftLowerLegBack": {
+            "id": 8,
+            "color": [48, 150, 224]
+        },
+        "rightFeet": {
+            "id": 9,
+            "color": [40, 163, 220]
+        },
+        "rightLowerLegFront": {
+            "id": 10,
+            "color": [33, 176, 214]
+        },
+        "leftFeet": {
+            "id": 11,
+            "color": [29, 188, 205]
+        },
+        "torsoFront": {
+            "id": 12,
+            "color": [26, 199, 194]
+        },
+        "torsoBack": {
+            "id": 13,
+            "color": [26, 210, 182]
+        },
+        "rightUpperArmFront": {
+            "id": 14,
+            "color": [28, 219, 169]
+        },
+        "rightUpperArmBack": {
+            "id": 15,
+            "color": [33, 227, 155]
+        },
+        "rightLowerArmBack": {
+            "id": 16,
+            "color": [41, 234, 141]
+        },
+        "leftLowerArmFront": {
+            "id": 17,
+            "color": [51, 240, 128]
+        },
+        "leftUpperArmFront": {
+            "id": 18,
+            "color": [64, 243, 116]
+        },
+        "leftUpperArmBack": {
+            "id": 19,
+            "color": [79, 246, 105]
+        },
+        "leftLowerArmBack": {
+            "id": 20,
+            "color": [96, 247, 97]
+        },
+        "rightHand": {
+            "id": 21,
+            "color": [115, 246, 91]
+        },
+        "rightLowerArmFront": {
+            "id": 22,
+            "color": [134, 245, 88]
+        },
+        "leftHand": {
+            "id": 23,
+            "color": [155, 243, 88]
+        }
+    }
 }
 
 class BodyPix {
@@ -81,50 +157,38 @@ class BodyPix {
     }
 
     /**
+     * Returns an rgb array
+     * @param {Object} a p5.Color obj
+     * @return {Array} an [r,g,b] array
+     */
+    /* eslint class-methods-use-this: "off" */
+    p5Color2RGB(p5ColorObj) {
+        const regExp = /\(([^)]+)\)/;
+        const match = regExp.exec(p5ColorObj.toString('rgb'));
+        const [r, g, b] = match[1].split(',')
+        return [r, g, b]
+    }
+
+    /**
      * Returns a bodyPartsSpec object 
      * @param {Array} an array of [r,g,b] colors
      * @return {object} an object with the bodyParts by color and id
      */
     /* eslint class-methods-use-this: "off" */
     bodyPartsSpec(colorOptions) {
-        const result = {};
-        let colors = colorOptions;
+        const result = colorOptions !== undefined || Object.keys(colorOptions).length >= 24 ? colorOptions : this.config.palette;
 
-        const bodyPartsIds = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-        const bodyPartsName = [
-            "none", "leftFace", "rightFace", "rightUpperLegFront", "rightLowerLegBack",
-            "rightUpperLegBack", "leftLowerLegFront", "leftUpperLegFront", "leftUpperLegBack", "leftLowerLegBack",
-            "rightFeet", "rightLowerLegFront", "leftFeet", "torsoFront", "torsoBack", "rightUpperArmFront", "rightUpperArmBack",
-            "rightLowerArmBack", "leftLowerArmFront", "leftUpperArmFront", "leftUpperArmBack", "leftLowerArmBack", "rightHand",
-            "rightLowerArmFront", "leftHand"
-        ];
-
-
-        // Check if we're getting p5 colors and return rgb
-        if (p5Utils.checkP5() && colors !== undefined && colors.length >= 24) {
-            const isP5Color = colors.every((color) => color instanceof window.p5.Color);
-            if (isP5Color === true) {
-                colors = colors.map((color) => {
-                    const regExp = /\(([^)]+)\)/;
-                    const match = regExp.exec(color.toString('rgb'));
-                    const [r, g, b] = match[1].split(',')
-                    return [r, g, b]
-                })
-            }
+        // Check if we're getting p5 colors, make sure they are rgb 
+        if (p5Utils.checkP5() && result !== undefined && Object.keys(result).length >= 24) {
+            // Ensure the p5Color object is an RGB array
+            Object.keys(result).forEach(part => {
+                if (result[part].color instanceof window.p5.Color) {
+                    result[part].color = this.p5Color2RGB(result[part].color);
+                } else {
+                    result[part].color = result[part].color;
+                }
+            });
         }
-
-        const palette = colors !== undefined || colors.length >= 24 ? colors : this.config.palette;
-
-        // Add DEFAULT_COLOR as result.palette;
-        result.palette = palette;
-        // Iterate over the bodyPartsName
-        // {"leftFace": {id: 0, color:[r, g, b]}, "rightFace": {id: 1, color:[r, g, b]}, ...}
-        bodyPartsName.forEach((part, idx) => {
-            result[part] = {
-                id: bodyPartsIds[idx],
-                color: palette[idx]
-            }
-        })
 
         return result;
     }
@@ -155,8 +219,10 @@ class BodyPix {
         const bodyPartsMeta = this.bodyPartsSpec(this.config.palette);
         const segmentation = await this.model.estimatePartSegmentation(imgToSegment, this.config.outputStride, this.config.segmentationThreshold);
 
+        const colorsArray = Object.keys(bodyPartsMeta).map(part => bodyPartsMeta[part].color)
+
         const result = {};
-        result.image = bp.toColoredPartImageData(segmentation, bodyPartsMeta.palette);
+        result.image = bp.toColoredPartImageData(segmentation, colorsArray);
         result.raw = segmentation;
         result.bodyParts = bodyPartsMeta;
 
