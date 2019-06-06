636d5bee6f34b4a06d7e22382a301216446bb70d
joeyklee <jsphknglee@gmail.com>
2019-06-06 13:12:26 -0400

    added tests

--- src/FaceApi/index_test.js
+++ src/FaceApi/index_test.js
@@ -1,11 +1,11 @@
-import { KeyObject } from "crypto";
-
 // Copyright (c) 2018 ml5
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-const { faceApi } = ml5;
+const {
+    faceApi
+} = ml5;
 
 const FACEAPI_DEFAULTS = {
     withFaceLandmarks: true,
@@ -22,45 +22,57 @@ const FACEAPI_DEFAULTS = {
 
 
 describe('faceApi', () => {
-  let faceapi;
+    let faceapi;
 
-  async function getImage() {
-    const img = new Image();
-    img.crossOrigin = true;
-    img.src = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models@master/tests/images/harriet_128x128.jpg';
-    await new Promise((resolve) => { img.onload = resolve; });
-    return img;
-  }
+    async function getImage() {
+        const img = new Image();
+        img.crossOrigin = true;
+        img.src = 'https://raw.githubusercontent.com/ml5js/ml5-examples/face-api/p5js/FaceApi/FaceApi_Image_Landmarks/assets/frida.jpg';
+        await new Promise((resolve) => {
+            img.onload = resolve;
+        });
+        return img;
+    }
 
-  async function getCanvas() {
-    const img = await getImage();
-    const canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
-    canvas.getContext('2d').drawImage(img, 0, 0);
-    return canvas;
-  }
+    async function getCanvas() {
+        const img = await getImage();
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        canvas.getContext('2d').drawImage(img, 0, 0);
+        return canvas;
+    }
 
-  beforeEach(async () => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
-    faceapi = await faceApi();
-  });
-
-  it('Should create faceApi with all the defaults', async () => {
-    expect(bp.config.withFaceLandmarks).toBe(FACEAPI_DEFAULTS.withFaceLandmarks);
-    expect(bp.config.withFaceExpressions).toBe(FACEAPI_DEFAULTS.withFaceExpressions);
-    expect(bp.config.withFaceDescriptors).toBe(FACEAPI_DEFAULTS.withFaceDescriptors);
-  });
-
-  describe('expressions', () => {
-    it('Should get expressions for Harriet Tubman', async () => {
-      const img = await getImage();
-      await faceapi.detectSingle(img)
-        .then(results => {
-            expect(results.expressions).any(Object);
-
-        })
+    beforeEach(async () => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+        faceapi = await faceApi();
     });
 
-  });
+    it('Should create faceApi with all the defaults', async () => {
+        expect(faceapi.config.withFaceLandmarks).toBe(FACEAPI_DEFAULTS.withFaceLandmarks);
+        expect(faceapi.config.withFaceExpressions).toBe(FACEAPI_DEFAULTS.withFaceExpressions);
+        expect(faceapi.config.withFaceDescriptors).toBe(FACEAPI_DEFAULTS.withFaceDescriptors);
+    });
+
+    describe('expressions', () => {
+        it('Should get expressions for Frida', async () => {
+            const img = await getImage();
+            await faceapi.detectSingle(img)
+                .then(results => {
+                    expect(results.expressions).toEqual(jasmine.any(Object));
+
+                })
+        });
+    });
+    
+    describe('landmarks', () => {
+        it('Should get landmarks for Frida', async () => {
+            const img = await getImage();
+            await faceapi.detectSingle(img)
+                .then(results => {
+                    expect(results.landmarks).toEqual(jasmine.any(Object));
+
+                })
+        });
+    });
 });
