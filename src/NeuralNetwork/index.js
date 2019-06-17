37414d768edb457502a804f3429b32aae4c3ee72
shiffman <daniel@shiffman.net>
2019-06-17 12:53:08 -0400

    proof of concept for training and prediction

--- src/NeuralNetwork/index.js
+++ src/NeuralNetwork/index.js
@@ -16,7 +16,6 @@ class NeuralNetwork {
    */
   constructor(options) {
     // TODO: create the model based on many more options and defaults
-    console.log(options);
 
     const inputUnits = options.input || 2;
     const hiddenUnits = Math.floor(inputUnits / 2) + 1;
@@ -40,9 +39,59 @@ class NeuralNetwork {
 
     this.model.compile({
       optimizer,
-      loss: 'categoricalCrossentropy',
+      loss: 'meanSquaredError',
       metrics: ['accuracy'],
     });
+
+    this.training_xs = [];
+    this.training_ys = [];
+  }
+
+  addData(xs, ys) {
+    this.training_xs.push(xs);
+    this.training_ys.push(ys);
+  }
+
+  async train(optionsOrEpochs, callback) {
+    let options;
+    if (optionsOrEpochs instanceof Object) {
+      options = optionsOrEpochs;
+    } else {
+      options = {
+        epochs: optionsOrEpochs,
+        shuffle: true,
+        validationSplit: 0.1,
+      };
+    }
+
+    const xs = tf.tensor2d(this.training_xs);
+    const ys = tf.tensor2d(this.training_ys);
+    await this.model.fit(xs, ys, {
+      shuffle: options.shuffle,
+      validationSplit: options.validationSplit,
+      epochs: options.epochs,
+      callbacks: {
+        onEpochEnd: (epoch, logs) => {
+          callback(null, { status: 'training', epoch, loss: logs.loss, logs });
+        },
+        onTrainEnd: () => {
+          callback(null, { status: 'complete' });
+        },
+      },
+    });
+    xs.dispose();
+    ys.dispose();
+  }
+
+  async predict(input, callback) {
+    const xs = tf.tensor2d([input]);
+    const ys = this.model.predict(xs);
+    const results = {
+      output: await ys.data(),
+      tensor: ys,
+    };
+    xs.dispose();
+    callback(null, results);
   }
 }
 
