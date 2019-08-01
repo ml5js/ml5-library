5c145d6a0407d3113c7d5ab3ee72157728d43545
joeyklee <jsphknglee@gmail.com>
2019-08-01 14:30:19 -0400

    adds this.multiPose() to else{}

--- src/PoseNet/index.js
+++ src/PoseNet/index.js
@@ -76,9 +76,11 @@ class PoseNet extends EventEmitter {
       }
       if (this.detectionType === 'single') {
         this.singlePose();
+      } else {
+        this.multiPose();
       }
 
-      this.multiPose();
+      
     }
     return this;
   }
