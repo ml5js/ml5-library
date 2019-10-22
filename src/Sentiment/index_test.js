ecd9029a1549c1fa92d3f1a02ac3b99f5317b8c9
yangyang <yangeon@163.com>
2019-10-22 01:41:31 -0400

    Removing unnecessary console.log.

--- src/Sentiment/index_test.js
+++ src/Sentiment/index_test.js
@@ -4,19 +4,17 @@ describe('Sentiment', ()=>{
   let model;
 
   beforeAll(async () => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     model = await sentiment('moviereviews').ready;
   });
 
   it("Model should be ready",()=> expect(model.ready).toBeTruthy());
 
   it("Happy has a sentiment score greater than 0.5", ()=>{
-    console.log(model.predict('Happy').score);
     expect(model.predict('Happy').score).toBeGreaterThan(0.5);
   });
 
   it("Shit has a sentiment score less than 0.5", ()=>{
-    console.log(model.predict('Shit').score);
     expect(model.predict('Shit').score).toBeLessThan(0.5);
   });
 });
