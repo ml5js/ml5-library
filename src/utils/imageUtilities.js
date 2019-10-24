2deeccc68bc9250e318156c8861845d0c1fdbcfd
joeyklee <jsphknglee@gmail.com>
2019-10-24 13:47:04 -0400

    adds flipImage to utils

--- src/utils/imageUtilities.js
+++ src/utils/imageUtilities.js
@@ -61,6 +61,56 @@ const cropImage = (img) => {
   return img.slice([beginHeight, beginWidth, 0], [size, size, 3]);
 };
 
+const flipImage = (img, options) => {
+  // image image, bitmap, or canvas
+  let imgWidth;
+  let imgHeight;
+  let inputImg;
+
+  if (img instanceof HTMLImageElement ||
+    img instanceof HTMLCanvasElement ||
+    img instanceof HTMLVideoElement ||
+    img instanceof ImageData) {
+    inputImg = img;
+  } else if (typeof img === 'object' &&
+    (img.elt instanceof HTMLImageElement ||
+      img.elt instanceof HTMLCanvasElement ||
+      img.elt instanceof HTMLVideoElement ||
+      img.elt instanceof ImageData)) {
+        
+    inputImg = img.elt; // Handle p5.js image
+  } else if (typeof img === 'object' &&
+    img.canvas instanceof HTMLCanvasElement) {
+    inputImg = img.canvas; // Handle p5.js image
+  } else {
+    inputImg = img;
+  }
+
+  if(inputImg instanceof HTMLVideoElement){
+    // should be videoWidth, videoHeight?
+    imgWidth = inputImg.width;
+    imgHeight = inputImg.height;
+  } else {
+    imgWidth = inputImg.width;
+    imgHeight = inputImg.height;
+  }
+
+
+  const canvas = document.createElement('canvas');
+  canvas.width = imgWidth;
+  canvas.height = imgHeight;
+
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(inputImg, 0, 0, imgWidth, imgHeight);
+  
+  if (options.flipped) {
+    ctx.scale(-1, 1);
+    ctx.drawImage(canvas, imgWidth * -1, 0);
+  } 
+
+  return {canvas, ctx};
+}
+
 // Static Method: image to tf tensor
 function imgToTensor(input, size = null) {
   return tf.tidy(() => {
@@ -79,4 +129,5 @@ export {
   processVideo,
   cropImage,
   imgToTensor,
+  flipImage
 };
