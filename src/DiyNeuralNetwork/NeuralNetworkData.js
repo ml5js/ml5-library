e6dcfa7037668948ad74c10f6860a87100c69bc7
joeyklee <jsphknglee@gmail.com>
2019-11-20 16:53:16 -0500

    adds blank functions in data class

--- src/DiyNeuralNetwork/NeuralNetworkData.js
+++ src/DiyNeuralNetwork/NeuralNetworkData.js
@@ -1,8 +1,56 @@
+// import * as tf from '@tensorflow/tfjs';
+// import callCallback from '../utils/callcallback';
+
 class NeuralNetworkData {
   constructor(){
-    this.temp = null;
+    this.data = {}
 
   }
+
+  // eslint-disable-next-line class-methods-use-this
+  normalizeData(){
+    
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  loadCsv(){
+
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  loadJSON(){
+
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  loadBlob(){
+
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  csvToJSON(){
+    
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  addData(){
+    
+  }
+
+
+  // eslint-disable-next-line class-methods-use-this
+  saveData(){
+
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  loadData(){
+    
+  }
+
+
+
+
 }
 
 export default NeuralNetworkData;
