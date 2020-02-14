2cd15a174824680f49e37ccc68a7b05c6edf0b91
Joey Lee <joeyklee@users.noreply.github.com>
2020-02-14 10:12:20 -0500

    adds community statement console log (#800)

---
+++ src/utils/community.js
@@ -0,0 +1,19 @@
+import { version } from '../../package.json';
+
+export default () => {
+console.log(`
+🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈
+🌟 Thank you for using ml5.js v${version} 🌟
+
+Please read our community statement to ensure 
+that the use of this software reflects the values 
+of the ml5.js community:
+↳ https://ml5js.org/about
+
+Reporting: 
+↳ https://github.com/ml5js/ml5-library/issues
+↳ Email: info@ml5js.org 
+🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈`)
+
+}
+
