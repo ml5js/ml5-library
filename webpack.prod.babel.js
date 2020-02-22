f9fc07762e2f839d3b4fadf03e052400e991fbf1
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-02-22 12:39:19 -0500

    Update build system to produce an unminified build of the ml5 library (in addition to the minified one)

--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 ml5
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
@@ -10,12 +10,20 @@ import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
 export default merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  output: {
-    filename: 'ml5.min.js'
+  entry: {
+    ml5: ["babel-polyfill", "./src/index.js"],
+    "ml5.min": ["babel-polyfill", "./src/index.js"],
   },
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    })
-  ]
-})
+  output: {
+    filename: "[name].js",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJSPlugin({
+        include: /\.min\.js$/,
+        sourceMap: true,
+      }),
+    ],
+  },
+});
