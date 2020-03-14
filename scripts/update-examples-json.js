898ffd302099716c57c42b0085bbf796382866c1
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-03-14 02:08:56 -0400

    Make cleanup-oriented changes to the update-examples-json.js file

--- scripts/update-examples-json.js
+++ scripts/update-examples-json.js
@@ -1,90 +1,88 @@
-const fs = require('fs');
-const path = require('path');
-const recursive = require("recursive-readdir");
+const fs = require("fs");
+const path = require("path");
 
-const baseurl = path.resolve(__dirname, "../examples");
+const baseURL = path.resolve(__dirname, "../examples");
+const webEditorURL = "https://editor.p5js.org/ml5/sketches";
 
-const webEditorURL = 'https://editor.p5js.org/ml5/sketches';
+const getDirectories = directoryPath => {
+  return fs.readdirSync(directoryPath).filter(file => {
+    return fs.statSync(`${directoryPath}/${file}`).isDirectory();
+  });
+};
 
-// Get all the references
-const p5Examples = getReferences(`${baseurl}/p5js`, 'p5js', webEditorURL);
-const plainJsExamples = getReferences(`${baseurl}/javascript`, 'javascript');
-const d3Examples = getReferences(`${baseurl}/d3`, 'd3');
-
-appendToSource(p5Examples, plainJsExamples);
-appendToSource(p5Examples, d3Examples);
-console.log(`Created/update examples index json!!`)
-
-// flatten the array to a json where each example parent is the key
-let output = {};
-p5Examples.forEach(item => {
-  output[item.parent] = item.children
-});
-
-// write out to file
-fs.writeFileSync(`${baseurl}/examples.json`, JSON.stringify(output), 'utf8')
-
-
-// TODO: functional javascription would be great! 
-function appendToSource(_source,_arr){
+// TODO: functional javascription would be great!
+function appendToSource(_source, _arr) {
   // Merge all of these examples to one file
   _arr.forEach(item => {
-    const {parent, children} = item;
+    const { parent, children } = item;
 
     // check if it exists
-    const match = _source.map( proj => proj.parent ).indexOf(parent);
-    if(match >= 0){
-      Object.entries(children).forEach( child => {
+    const match = _source.map(proj => proj.parent).indexOf(parent);
+    if (match >= 0) {
+      Object.entries(children).forEach(child => {
         const k = child[0];
         const v = child[1];
         _source[match].children[k] = v;
-      })
+      });
     }
-  })
-}
-
-
-function getDirectories(path) {
-  return fs.readdirSync(path).filter(function (file) {
-    return fs.statSync(path+'/'+file).isDirectory();
   });
 }
 
-function getReferences(_examplesRoot, _rootName, _webEditorURL = null){
+function getReferences(_examplesRoot, _rootName, _webEditorURL = null) {
   const dirs = getDirectories(_examplesRoot);
 
   const subdirs = dirs.map(dir => {
-      let items = getDirectories(`${_examplesRoot}/${dir}`);
+    const items = getDirectories(`${_examplesRoot}/${dir}`);
 
-      const localItems = items.map(item => { 
-        return { 
-          name: item, 
-          url:`../${_rootName}/${dir}/${item}`} 
+    const localItems = items.map(item => {
+      return {
+        name: item,
+        url: `../${_rootName}/${dir}/${item}`,
+      };
+    });
+
+    if (_webEditorURL !== null) {
+      const webEditorItems = items.map(item => {
+        return {
+          name: item,
+          url: `${_webEditorURL}/${item}`,
+        };
       });
 
-      if(_webEditorURL !== null){
-        const webEditorItems = items.map(item => {
-          return {
-            name: item,
-            url: `${_webEditorURL}/${item}`
-          }
-        });
+      return {
+        parent: dir,
+        children: {
+          [_rootName]: localItems,
+          p5webeditor: webEditorItems,
+        },
+      };
+    }
 
-        return {
-          parent: dir, 
-          children: {
-            [_rootName]: localItems,
-            "p5webeditor": webEditorItems
-          }}
-      } else {
-        return {
-          parent: dir, 
-          children: {
-            [_rootName]: localItems
-          }}
-      }
-      
+    return {
+      parent: dir,
+      children: {
+        [_rootName]: localItems,
+      },
+    };
   });
 
   return subdirs;
 }
+
+// Get all the references
+const p5Examples = getReferences(`${baseURL}/p5js`, "p5js", webEditorURL);
+const plainJsExamples = getReferences(`${baseURL}/javascript`, "javascript");
+const d3Examples = getReferences(`${baseURL}/d3`, "d3");
+
+appendToSource(p5Examples, plainJsExamples);
+appendToSource(p5Examples, d3Examples);
+console.log(`Created/update examples index json!!`);
+
+// flatten the array to a json where each example parent is the key
+const output = {};
+p5Examples.forEach(item => {
+  output[item.parent] = item.children;
+});
+
+// write out to file
+fs.writeFileSync(`${baseURL}/examples.json`, JSON.stringify(output), "utf8");
