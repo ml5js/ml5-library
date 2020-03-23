84cfa36a40a90abdb7feac61c59716cbe7a66842
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-03-22 23:20:19 -0400

    Add a simple version of the Handpose model

---
+++ examples/p5js/Handpose/Handpose_Webcam/sketch.js
@@ -0,0 +1,45 @@
+let handpose;
+let video;
+let poses = [];
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  handpose = ml5.handpose(video, modelReady);
+
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  handpose.on("pose", results => {
+    poses = results;
+    console.log(poses);
+  });
+
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  console.log("Model ready!");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  for (let i = 0; i < poses.length; i += 1) {
+    const pose = poses[i];
+    for (let j = 0; j < pose.landmarks.length; j += 1) {
+      const keypoint = pose.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(keypoint[0], keypoint[1], 10, 10);
+    }
+  }
+}
