8da1b9ccb4b5f6c8a76405f3ac92dfa81b8e429b
joeyklee <joeyklee@nyu.edu>
2020-04-07 11:26:38 -0400

    updates basic neuroevolution example

--- examples/p5js/NeuralNetwork/NeuroEvolution_basics/sketch.js
+++ examples/p5js/NeuralNetwork/NeuroEvolution_basics/sketch.js
@@ -1,9 +1,9 @@
 let meta = {
   "inputUnits": [1],
   "outputUnits": 2,
-  "inputs": { "0": { "dtype": "number", "min": 0.1, "max": 0.9 } },
+  "inputs": { "direction": { "dtype": "number", "min": 0.1, "max": 0.9 } },
   "outputs": {
-    "0": {
+    "label": {
       "dtype": "string",
       "min": 0,
       "max": 1,
@@ -23,10 +23,14 @@ function setup() {
   }
   // Create Neural Network
   const nn = ml5.neuralNetwork(options);
-  nn.addData([0.1], ['left'])
-  nn.addData([0.9], ['right'])
-  nn.createMetaData(nn.neuralNetworkData.data.raw)
+
+  // nn.addData([0.1], ['left'])
+  // nn.addData([0.9], ['right'])
+  // nn.createMetaData(nn.neuralNetworkData.data.raw)
+  
   nn.addDefaultLayers('classification', meta);
+  nn.neuralNetworkData.meta = meta;
+  console.log(nn)
   nn.classify([0.2], (err, result) => {
     if (err) console.log(err);
     console.log(result);
