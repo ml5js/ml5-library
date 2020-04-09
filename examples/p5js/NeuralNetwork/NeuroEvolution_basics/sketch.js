7a0e328a174535be6de04aafae6632c8cf523f42
joeyklee <joeyklee@nyu.edu>
2020-04-09 18:49:44 -0400

    fix merge conflict

--- examples/p5js/NeuralNetwork/NeuroEvolution_basics/sketch.js
+++ examples/p5js/NeuralNetwork/NeuroEvolution_basics/sketch.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-let meta = {
-  "inputUnits": [1],
-  "outputUnits": 2,
-  "inputs": { "direction": { "dtype": "number", "min": 0.1, "max": 0.9 } },
-  "outputs": {
-    "label": {
-      "dtype": "string",
-      "min": 0,
-      "max": 1,
-      "uniqueValues": ["left", "right"],
-      "legend": { "left": [1,0], "right": [0,1]}
-    }
-  },
-  "isNormalized": false
-};
-=======
 // let meta = {
 //   "inputUnits": [3],
 //   "outputUnits": 2,
@@ -40,7 +23,6 @@ let meta = {
 //   },
 //   "isNormalized": false
 // };
->>>>>>> a281a65a03509597131d9154cb4eef80cbfffe9f
 
 function setup() {
   const options = {
@@ -49,20 +31,6 @@ function setup() {
     task: 'classification'
   }
   const nn = ml5.neuralNetwork(options);
-<<<<<<< HEAD
-
-  // nn.addData([0.1], ['left'])
-  // nn.addData([0.9], ['right'])
-  // nn.createMetaData(nn.neuralNetworkData.data.raw)
-  
-  nn.addDefaultLayers('classification', meta);
-  nn.neuralNetworkData.meta = meta;
-  console.log(nn)
-  nn.classify([0.2], (err, result) => {
-    if (err) console.log(err);
-    console.log(result);
-  });
-=======
   nn.buildModelBasedOnNothing();
 
   const results1 = nn.classifySync([0.2, 0.1, 0.5]);
@@ -83,5 +51,4 @@ function setup() {
   //   if (err) console.log(err);
   //   console.log(result);
   // });
->>>>>>> a281a65a03509597131d9154cb4eef80cbfffe9f
 }
