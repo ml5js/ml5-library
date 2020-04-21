14a32f83e44624b56b231d73f17aaa3b0b92312d
joeyklee <joeyklee@nyu.edu>
2020-04-21 19:41:21 -0400

    updating webcam example

--- examples/p5js/BodyPix/BodyPix_Webcam/sketch.js
+++ examples/p5js/BodyPix/BodyPix_Webcam/sketch.js
@@ -1,37 +1,40 @@
 let bodypix;
 let video;
 let segmentation;
-let img;
 
 const options = {
-    outputStride: 8, // 8, 16, or 32, default is 16
-    segmentationThreshold: 0.3 // 0 - 1, defaults to 0.5 
-}
+  outputStride: 16, // 8, 16, or 32, default is 16
+  segmentationThreshold: 0.5, // 0 - 1, defaults to 0.5
+};
 
-function preload(){
-  bodypix = ml5.bodyPix(options);
+function preload() {
+    bodypix = ml5.bodyPix(options);
 }
 
 function setup() {
-    createCanvas(320, 240);
-    // load up your video
-    video = createCapture(VIDEO);
-    video.size(width, height);
-    // video.hide(); // Hide the video element, and just show the canvas
-    bodypix.segment(video, gotResults)
+  createCanvas(320, 240);
+  // load up your video
+  video = createCapture(VIDEO, () => {
+    // video.size(width, height);
+    video.hide(); // Hide the video element, and just show the canvas
+    segment();
+  });
+}
+
+function segment() {
+  bodypix.segment(video, gotResults);
 }
 
 function gotResults(err, result) {
-    if (err) {
-        console.log(err)
-        return
-    }
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    segmentation = result;
+  segmentation = result;
 
-    background(0);
-    image(segmentation.backgroundMask, 0, 0, width, height)
+  background(0);
+  image(segmentation.backgroundMask, 0, 0, width, height);
 
-    bodypix.segment(video, gotResults)
-
+  segment();
 }
