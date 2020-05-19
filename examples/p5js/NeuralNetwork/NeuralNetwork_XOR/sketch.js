0dda94b5d25b784526994f1ce33dcec2ed4f1f54
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-05-19 08:55:48 -0400

    Fix j++ unary operators

--- examples/p5js/NeuralNetwork/NeuralNetwork_XOR/sketch.js
+++ examples/p5js/NeuralNetwork/NeuralNetwork_XOR/sketch.js
@@ -13,14 +13,14 @@ function setup() {
 
   let index = 0;
   for (let i = 0; i < cols; i += 1) {
-    for (let j = 0; j < rows; j++) {
+    for (let j = 0; j < rows; j += 1) {
       const br = 255; // y_values[index] * 255
       fill(br);
       rect(i * resolution, j * resolution, resolution, resolution);
       fill(255 - br);
       textSize(8);
       textAlign(CENTER, CENTER);
-      text(nf(0.5, 1, 2), i * resolution + resolution / 2, j * resolution + resolution / 2)
+      text(nf(0.5, 1, 2), i * resolution + resolution / 2, j * resolution + resolution / 2);
       // text(nf(y_values[index], 1, 2), i * resolution + resolution / 2, j * resolution + resolution / 2)
       index++;
     }
@@ -30,10 +30,10 @@ function setup() {
     inputs: 2,
     outputs: 1,
     learningRate: 0.25,
-    task:'regression',
-    debug:true
+    task: "regression",
+    debug: true,
     // hiddenUnits: 2
-  }
+  };
   model = ml5.neuralNetwork(options);
 
   // model = ml5.neuralNetwork(2, 1);
@@ -44,9 +44,7 @@ function setup() {
 
   model.normalizeData();
   model.train({ epochs: 50 }, whileTraining, finishedTraining);
-
 }
-
 
 function whileTraining(epoch, logs) {
   console.log(`Epoch: ${epoch} - loss: ${logs.loss.toFixed(2)}`);
@@ -57,7 +55,7 @@ function finishedTraining() {
   // TODO: Support prediction on multiple rows of input data
   const xs = [];
   for (let i = 0; i < cols; i += 1) {
-    for (let j = 0; j < rows; j++) {
+    for (let j = 0; j < rows; j += 1) {
       const x1 = i / cols;
       const x2 = j / rows;
       xs.push([x1, x2]);
@@ -69,22 +67,26 @@ function finishedTraining() {
 
 function gotResults(error, results) {
   background(0);
-  if(error){
-    console.log(err)
-    return
+  if (error) {
+    console.log(err);
+    return;
   }
   // console.log(results);
   let index = 0;
   for (let i = 0; i < cols; i += 1) {
-    for (let j = 0; j < rows; j++) {
+    for (let j = 0; j < rows; j += 1) {
       const br = results[index][0].value * 255; // y_values[index] * 255
       fill(br);
       rect(i * resolution, j * resolution, resolution, resolution);
       fill(255 - br);
       textSize(8);
       textAlign(CENTER, CENTER);
-      text(nf(0.5, 1, 2), i * resolution + resolution / 2, j * resolution + resolution / 2)
-      text(nf(results[index][0].value, 1, 2), i * resolution + resolution / 2, j * resolution + resolution / 2)
+      text(nf(0.5, 1, 2), i * resolution + resolution / 2, j * resolution + resolution / 2);
+      text(
+        nf(results[index][0].value, 1, 2),
+        i * resolution + resolution / 2,
+        j * resolution + resolution / 2,
+      );
       index++;
     }
   }
