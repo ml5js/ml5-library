d565b94644386cb177d637a11c318b2881f9e45f
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-05-19 08:48:51 -0400

    Fix lint errors in YOLO_single_image sketch and CVAE

--- examples/p5js/YOLO/YOLO_single_image/sketch.js
+++ examples/p5js/YOLO/YOLO_single_image/sketch.js
@@ -7,22 +7,21 @@ let status;
 
 function setup() {
   createCanvas(640, 420);
-  img = createImg('images/cat2.JPG', imageReady);
+  img = createImg("images/cat2.JPG", imageReady);
   img.hide();
   img.size(640, 420);
-
 }
 
 // Change the status when the model loads.
 function modelReady() {
-  console.log("model Ready!")
+  console.log("model Ready!");
   status = true;
 }
 
 // When the image has been loaded,
 // get a prediction for that image
 function imageReady() {
-  console.log('Detecting') 
+  console.log("Detecting");
   yolo.detect(img, gotResult);
 }
 
@@ -31,24 +30,32 @@ function gotResult(err, results) {
   if (err) {
     console.log(err);
   }
-  console.log(results)
+  console.log(results);
   objects = results;
 }
 
-
 function draw() {
   // unless the model is loaded, do not draw anything to canvas
-  if (status != undefined) {
-    image(img, 0, 0)
+  if (status !== undefined) {
+    image(img, 0, 0);
 
-    for (let i = 0; i < objects.length; i++) {
+    for (let i = 0; i < objects.length; i += 1) {
       noStroke();
       fill(0, 255, 0);
-      text(objects[i].label + " " + nfc(objects[i].confidence * 100.0, 2) + "%", objects[i].x * width + 5, objects[i].y * height + 15);
+      text(
+        `${objects[i].label} ${nfc(objects[i].confidence * 100.0, 2)}%`,
+        objects[i].x * width + 5,
+        objects[i].y * height + 15,
+      );
       noFill();
       strokeWeight(4);
       stroke(0, 255, 0);
-      rect(objects[i].x * width, objects[i].y * height, objects[i].w * width, objects[i].h * height);
+      rect(
+        objects[i].x * width,
+        objects[i].y * height,
+        objects[i].w * width,
+        objects[i].h * height,
+      );
     }
   }
 }
