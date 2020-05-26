8f5ab174874a14150a49babd3f11c1a2e5ea16d8
joeyklee <joeyklee@nyu.edu>
2020-05-26 19:47:30 -0400

    testing moving cocossd test to feature root dir

--- src/ObjectDetector/CocoSsd/index_test.js
+++ src/ObjectDetector/CocoSsd/index_test.js
@@ -5,66 +5,67 @@
 
 // const { objectDetector } = ml5;
 
-const COCOSSD_DEFAULTS = {
-  base: 'lite_mobilenet_v2',
-  modelUrl: undefined,
-}
+// const COCOSSD_DEFAULTS = {
+//   base: 'lite_mobilenet_v2',
+//   modelUrl: undefined,
+// }
 
-describe('objectDetector with cocossd', () => {
-  let cocoDetector;
+// describe('objectDetector with cocossd', () => {
+//   let cocoDetector;
 
-  async function getRobin() {
-    const img = new Image();
-    img.crossOrigin = '';
-    img.src = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-library@development/assets/bird.jpg';
-    await new Promise((resolve) => {
-      img.onload = resolve;
-    });
-    return img;
-  }
+//   async function getRobin() {
+//     const img = new Image();
+//     img.crossOrigin = '';
+//     img.src = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-library@development/assets/bird.jpg';
+//     await new Promise((resolve) => {
+//       img.onload = resolve;
+//     });
+//     return img;
+//   }
 
-  async function getImageData() {
-    const arr = new Uint8ClampedArray(20000);
+//   async function getImageData() {
+//     const arr = new Uint8ClampedArray(20000);
 
-    // Iterate through every pixel
-    for (let i = 0; i < arr.length; i += 4) {
-      arr[i + 0] = 0; // R value
-      arr[i + 1] = 190; // G value
-      arr[i + 2] = 0; // B value
-      arr[i + 3] = 255; // A value
-    }
+//     // Iterate through every pixel
+//     for (let i = 0; i < arr.length; i += 4) {
+//       arr[i + 0] = 0; // R value
+//       arr[i + 1] = 190; // G value
+//       arr[i + 2] = 0; // B value
+//       arr[i + 3] = 255; // A value
+//     }
 
-    // Initialize a new ImageData object
-    const img = new ImageData(arr, 200);
-    return img;
-  }
+//     // Initialize a new ImageData object
+//     const img = new ImageData(arr, 200);
+//     return img;
+//   }
 
-  describe('cocossd', () => {
+//   describe('cocossd', () => {
 
-    beforeAll(async () => {
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
-      cocoDetector = await ml5.objectDetector('cocossd');
-    });
-
-
-    it('Should instantiate with the following defaults', () => {
-      expect(cocoDetector.config.base).toBe(COCOSSD_DEFAULTS.base);
-      expect(cocoDetector.config.modelUrl).toBe(COCOSSD_DEFAULTS.modelUrl);
-    });
-
-    it('detects a robin', async () => {
-      const robin = await getRobin();
-      const detection = await cocoDetector.detect(robin);
-      expect(detection[0].label).toBe('bird');
-    });
-
-    it('detects takes ImageData', async () => {
-      const img = await getImageData();
-      const detection = await cocoDetector.detect(img);
-      expect(detection).toEqual([]);
-    });
+//     beforeAll(async () => {
+//       jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
+//       cocoDetector = await ml5.objectDetector('cocossd');
+//       console.log(cocoDetector)
+//     });
 
 
-  });
+//     it('Should instantiate with the following defaults', () => {
+//       expect(cocoDetector.config.base).toBe(COCOSSD_DEFAULTS.base);
+//       expect(cocoDetector.config.modelUrl).toBe(COCOSSD_DEFAULTS.modelUrl);
+//     });
 
-});
+//     it('detects a robin', async () => {
+//       const robin = await getRobin();
+//       const detection = await cocoDetector.detect(robin);
+//       expect(detection[0].label).toBe('bird');
+//     });
+
+//     it('detects takes ImageData', async () => {
+//       const img = await getImageData();
+//       const detection = await cocoDetector.detect(img);
+//       expect(detection).toEqual([]);
+//     });
+
+
+//   });
+
+// });
