e7ec2c5461d1f5ea4f410dad58a00f8bd6958242
joeyklee <joeyklee@nyu.edu>
2020-05-27 18:52:00 -0400

    moves model tests into root

--- src/ObjectDetector/index_test.js
+++ src/ObjectDetector/index_test.js
@@ -26,65 +26,100 @@
 // });
 
 const COCOSSD_DEFAULTS = {
-  base: 'lite_mobilenet_v2',
+  base: "lite_mobilenet_v2",
   modelUrl: undefined,
+};
+
+const YOLO_DEFAULTS = {
+  IOUThreshold: 0.4,
+  classProbThreshold: 0.4,
+  filterBoxesThreshold: 0.01,
+  size: 416,
+};
+
+async function getRobin() {
+  const img = new Image();
+  img.crossOrigin = "";
+  img.src = "https://cdn.jsdelivr.net/gh/ml5js/ml5-library@development/assets/bird.jpg";
+  await new Promise(resolve => {
+    img.onload = resolve;
+  });
+  return img;
 }
 
-describe('objectDetector with cocossd', () => {
+async function getImageData() {
+  const arr = new Uint8ClampedArray(20000);
+
+  // Iterate through every pixel
+  for (let i = 0; i < arr.length; i += 4) {
+    arr[i + 0] = 0; // R value
+    arr[i + 1] = 190; // G value
+    arr[i + 2] = 0; // B value
+    arr[i + 3] = 255; // A value
+  }
+
+  // Initialize a new ImageData object
+  const img = new ImageData(arr, 200);
+  return img;
+}
+
+describe("objectDetector", () => {
   let cocoDetector;
+  let yolo;
 
-  async function getRobin() {
-    const img = new Image();
-    img.crossOrigin = '';
-    img.src = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-library@development/assets/bird.jpg';
-    await new Promise((resolve) => {
-      img.onload = resolve;
-    });
-    return img;
-  }
-
-  async function getImageData() {
-    const arr = new Uint8ClampedArray(20000);
-
-    // Iterate through every pixel
-    for (let i = 0; i < arr.length; i += 4) {
-      arr[i + 0] = 0; // R value
-      arr[i + 1] = 190; // G value
-      arr[i + 2] = 0; // B value
-      arr[i + 3] = 255; // A value
-    }
-
-    // Initialize a new ImageData object
-    const img = new ImageData(arr, 200);
-    return img;
-  }
-
-  describe('cocossd', () => {
-
+  /**
+   * Test cocossd object detector
+   */
+  describe("objectDetector: cocossd", () => {
     beforeAll(async () => {
       jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
-      cocoDetector = await ml5.objectDetector('cocossd');
+      cocoDetector = await ml5.objectDetector("cocossd");
     });
 
-
-    it('Should instantiate with the following defaults', () => {
+    it("Should instantiate with the following defaults", () => {
       expect(cocoDetector.config.base).toBe(COCOSSD_DEFAULTS.base);
       expect(cocoDetector.config.modelUrl).toBe(COCOSSD_DEFAULTS.modelUrl);
     });
 
-    it('detects a robin', async () => {
+    it("detects a robin", async () => {
       const robin = await getRobin();
       const detection = await cocoDetector.detect(robin);
-      expect(detection[0].label).toBe('bird');
+      expect(detection[0].label).toBe("bird");
     });
 
-    it('detects takes ImageData', async () => {
+    it("detects takes ImageData", async () => {
       const img = await getImageData();
       const detection = await cocoDetector.detect(img);
       expect(detection).toEqual([]);
     });
-
-
   });
 
+  /**
+   * Test YOLO object detector
+   */
+  describe("objectDetector: yolo", () => {
+    beforeAll(async () => {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
+      yolo = await ml5.objectDetector("yolo", { disableDeprecationNotice: true, ...YOLO_DEFAULTS });
+    });
+
+    it("instantiates the YOLO classifier with defaults", () => {
+      expect(yolo.IOUThreshold).toBe(YOLO_DEFAULTS.IOUThreshold);
+      expect(yolo.classProbThreshold).toBe(YOLO_DEFAULTS.classProbThreshold);
+      expect(yolo.filterBoxesThreshold).toBe(YOLO_DEFAULTS.filterBoxesThreshold);
+      expect(yolo.size).toBe(YOLO_DEFAULTS.size);
+    });
+
+    it("detects a robin", async () => {
+      const robin = await getRobin();
+      const detection = await yolo.detect(robin);
+      expect(detection[0].label).toBe("bird");
+    });
+
+    it("detects takes ImageData", async () => {
+      const img = await getImageData();
+      const detection = await yolo.detect(img);
+      expect(detection).toEqual([]);
+    });
+  });
 });
