d412a5d3c55bdc82be411ce60c14f42462c1bde5
Daniel Shiffman <daniel@shiffman.net>
2020-09-08 17:15:50 -0400

    minor change for consistency
    This is a tiny tweak of the example to match how the code is shown / explained on the Teachable Machine website itself. The URL that you are given to copy does not include `model.json`.

--- examples/p5js/TeachableMachine/ImageModel_TM/sketch.js
+++ examples/p5js/TeachableMachine/ImageModel_TM/sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-const imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/model.json';
+const imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
 
 // Video
 let video;
@@ -22,7 +22,7 @@ let label = "";
 
 // Load the model first
 function preload() {
-  classifier = ml5.imageClassifier(imageModelURL);
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 }
 
 function setup() {
