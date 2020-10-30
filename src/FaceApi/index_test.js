a80ef2b1aa81b2ba149f8ec83b9a5b5024941dd4
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-10-30 10:10:10 -0400

    Change FaceAPI to reference models on the main branch of ml5 data and models

--- src/FaceApi/index_test.js
+++ src/FaceApi/index_test.js
@@ -14,15 +14,15 @@ const FACEAPI_DEFAULTS = {
   withTinyNet: true,
   MODEL_URLS: {
     Mobilenetv1Model:
-      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/ssd_mobilenetv1_model-weights_manifest.json",
+      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/main/models/faceapi/ssd_mobilenetv1_model-weights_manifest.json",
     TinyFaceDetectorModel:
-      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/tiny_face_detector_model-weights_manifest.json",
+      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/main/models/faceapi/tiny_face_detector_model-weights_manifest.json",
     FaceLandmarkModel:
-      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/face_landmark_68_model-weights_manifest.json",
+      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/main/models/faceapi/face_landmark_68_model-weights_manifest.json",
     FaceLandmark68TinyNet:
-      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/face_landmark_68_tiny_model-weights_manifest.json",
+      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/main/models/faceapi/face_landmark_68_tiny_model-weights_manifest.json",
     FaceRecognitionModel:
-      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/face_recognition_model-weights_manifest.json",
+      "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/main/models/faceapi/face_recognition_model-weights_manifest.json",
   },
 };
 
