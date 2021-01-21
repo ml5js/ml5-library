f0a0ff720772abaf700cf11fa29887799c2a9100
Tom-Lucas Säger <tlsaeger@icloud.com>
2021-01-21 13:38:33 +0100

    fixed error in comment

--- examples/p5js/Facemesh/Facemesh_Webcam/sketch.js
+++ examples/p5js/Facemesh/Facemesh_Webcam/sketch.js
@@ -26,7 +26,7 @@ function modelReady() {
 function draw() {
   image(video, 0, 0, width, height);
 
-  // We can call both functions to draw all keypoints
+  // We call function to draw all keypoints
   drawKeypoints();
 }
 
