c32aae336cebf77dc9088a888b042ede6d670700
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-02-28 11:58:34 -0500

    Add Boilerplate examples for p5.js and javascript

---
+++ examples/javascript/ml5Boilerplate/ml5Boilerplate_Version/sketch.js
@@ -0,0 +1,7 @@
+// Your code will go here
+
+// Open up your console - if everything loaded properly you should see the version number 
+// corresponding to the latest version of ml5 printed to the console and on the screen.
+console.log('ml5 version:', ml5.version);
+
+document.body.append(document.createTextNode(ml5.version));
