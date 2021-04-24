384a4213ec9de822116ed95ba489906f726316eb
Linda Paiste <lindapaiste@gmail.com>
2021-04-24 16:08:44 -0500

    Improved JSDoc, fixed warnings about redundant variables.

--- src/KMeans/index.js
+++ src/KMeans/index.js
@@ -5,35 +5,51 @@
 
 /* eslint "no-param-reassign": [2, { "props": false }] */
 /*
-K-Means Algorithm (with Euclidian distance).
+K-Means Algorithm (with Euclidean distance).
 */
 
 import * as tf from '@tensorflow/tfjs';
 import callCallback from '../utils/callcallback';
-import {
-  randomSample
-} from '../utils/random';
+import {randomSample} from '../utils/random';
 
+/**
+ * @typedef {Object} KMeansOptions
+ * @property {number} k - The number of clusters. Default 3.
+ * @property {number} maxIter - Max number of iterations to try before forcing convergence. Default 5.
+ * @property {number} threshold - Threshold for updated centroid distance before declaring convergence. Default 0.5
+ */
+
+/**
+ * @type {KMeansOptions}
+ */
 const DEFAULTS = {
   'k': 3,
   'maxIter': 5,
   'threshold': 0.5,
 };
 
+// TODO: can maybe combine with csv read in NeuralNetwork
 /**
  * Read in a csv file from a path to its location.
- * @param {string} path 
+ * @param {string} path
+ * @return {Promise<tf.TensorContainer[]>}
  */
 async function readCsv(path) {
   const myCsv = tf.data.csv(path);
-  const loadedData = await myCsv.toArray();
-  return loadedData;
+  return myCsv.toArray();
 }
+
+/**
+ * A valid dataset is an array of x,y coordinates.  Each element of the array should be an object with numeric
+ * properties like `{ x: 0, y: 0 }`, or an array of numbers like `[0, 0]`.
+ * @typedef {Array<(Record<any, number> | Array<number>)>} KMeansDataSet
+ */
 
 /**
  * Load and flatten an array of arrays, an array of objects, or a string
  *   path to a csv.
- * @param {string || array || object} inputData 
+ * @param {(string|KMeansDataSet)} inputData
+ * @return {number[][]}
  */
 async function loadDataset(inputData) {
   let data;
@@ -42,119 +58,135 @@ async function loadDataset(inputData) {
   } else {
     data = inputData;
   }
-  const dataFlat = data.map((d) => {
+  return data.map((d) => {
     return Object.values(d)
   });
-  return dataFlat;
 }
 
-
+/**
+ * // listed on https://learn.ml5js.org/
+ * @property {KMeansOptions} config
+ * @property {Array<[number, number] & {tensor: tf.Tensor<tf.Rank.R1>, centroid: number}>} dataset
+ * @property {tf.Tensor} dataTensor
+ * @property {tf.Tensor2D} centroids
+ *
+ * // not listed
+ * @property {Promise} ready
+ * @property {tf.Tensor2D} centroidsOld
+ */
 class KMeans {
   /**
    * Create a K-Means.
-   * @param {String || array || object} dataset - The dataset to cluster.
-   * @param {options} options - An object describing a model's parameters:
+   * @param {(string | KMeansDataSet)} dataset - The dataset to cluster.
+   * @param {Partial<KMeansOptions>} [options] - An object describing a model's parameters:
    *    - k: number of clusters
    *    - maxIter: Max number of iterations to try before forcing convergence.
-   *    - threshold: Threshold for updated centriod distance before declaring convergence.
-   * @param {function} callback  - Optional. A callback to be called once 
+   *    - threshold: Threshold for updated centroid distance before declaring convergence.
+   * @param {function} [callback] - Optional. A callback to be called once
    *    the model has loaded. If no callback is provided, it will return a 
    *    promise that will be resolved once the model has loaded.
    */
-  constructor(dataset, options, callback) {
+  constructor(dataset, options = {}, callback) {
     this.config = {
-      k: options.k || DEFAULTS.k,
-      maxIter: options.maxIter || DEFAULTS.maxIter,
-      threshold: options.threshold || DEFAULTS.threshold
+      ...options,
+      ...DEFAULTS
     };
     this.ready = callCallback(this.load(dataset), callback);
   }
 
   /**
    * Load dataset, find initial centroids, and run model.
-   * @param {string || array || object} dataset 
+   * Returns a Promise which resolves to the model instance when ready
+   *
+   * @public
+   * @param {(string | KMeansDataSet)} dataset
+   * @return {Promise<this>}
    */
   async load(dataset) {
-    
     this.dataset = await loadDataset(dataset);
     tf.tidy( () => {
       this.dataTensor = tf.tensor2d(this.dataset);
       this.dataset.forEach(d => {
-        const tensors = tf.tensor1d(Object.values(d));
-        d.tensor = tensors;
+        // note: Object.values(d) will not work if d.tensor and/or d.centroid have already been set
+        // but it is ok here because this.dataset was just set
+        d.tensor = tf.tensor1d(Object.values(d));
       });
       this.centroids = tf.tensor2d(randomSample(this.dataset, this.config.k, false));
       this.fit();
     })
-  
     return this;
   }
 
   /**
    * Run K-Means algorithm.
+   * @private
    */
   fit() {
     this.getClosestCentroids()
     this.recenterCentroids();
-    let centroidDistance = KMeans.getEuclidianDistance(this.centroids, this.centroidsOld);
+    let centroidDistance = KMeans.getEuclideanDistance(this.centroids, this.centroidsOld);
     let iteration = 0;
     while (centroidDistance > this.config.threshold && iteration < this.config.maxIter) {
       this.getClosestCentroids();
       this.recenterCentroids();
-      centroidDistance = KMeans.getEuclidianDistance(this.centroids, this.centroidsOld);
+      centroidDistance = KMeans.getEuclideanDistance(this.centroids, this.centroidsOld);
       iteration += 1;
     }
   }
 
   /**
-   * Find closest centroids to each observation and store as attribute.
+   * Find closest centroids to each observation and store as an attribute.
+   * @private
    */
   getClosestCentroids() {
     // find closest initial tensor
     this.dataset.forEach(d => {
-      const minCentroid = this.closestCentroid(d.tensor);
-      d.centroid = minCentroid;
+      d.centroid = this.closestCentroid(d.tensor);
     })
   }
 
   /**
-   * Load and flatten an array of arrays, an array of objects, or a string
-   *   path to a csv.
-   * @param {string || array || object} inputData 
+   * Find closest centroid for a given tensor
+   * @private
+   * @param {tf.Tensor} dataTensor
+   * @return {number}
    */
   closestCentroid(dataTensor) {
     return tf.tidy(() => {
       const dist = this.centroids.squaredDifference(dataTensor).sum(1).sqrt();
-      const minCentroid = dist.argMin().arraySync();
-      return minCentroid;
+      return dist.argMin().arraySync();
     });
   }
 
   /**
-   * Assing `value` to a cluster.
-   * @param {array || object} value 
+   * Assign `value` to a cluster.
+   * @private
+   * Note: Cannot make public because of Error when calling: "Tensor is disposed" on this.centroids.  Would need work.
+   *
+   * @param {(number[] | Record<any, number>)} value - a single point object `{ x: 0, y: 0 }` or tuple `[0, 0]`
+   * @return
    */
   classify(value) {
     return tf.tidy(() => {
       // input must be array or object
       const valueTensor = tf.tensor1d(Object.values(value));
-      const minCentroid = this.closestCentroid(valueTensor);
-      return minCentroid;
+      return this.closestCentroid(valueTensor);
     })
   }
 
   /**
    * Recenter each centroid.
+   * @private
    */
   recenterCentroids() {
     // store previous run's centroids for convergence
     // recenter each centroid
-    const result = tf.tidy( () => {
+    this.centroids = tf.tidy(() => {
       this.centroidsOld = this.centroids;
       return tf.stack(this.centroids.unstack().map((centroid, k) => {
         // subset centroid to its cluster
         const centroidK = this.dataset.filter(d => d.centroid === k);
-        // conver to tensor
+        // convert to tensor
         const centroidKTensor = centroidK.map(d => d.tensor);
         if (centroidKTensor.length === 0) {
           return centroid;
@@ -162,20 +194,20 @@ class KMeans {
           return centroidKTensor[0];
         }
         // grab mean for for cluster
-        const newCentroids = tf.tidy(() => tf.stack(centroidKTensor).mean(0));
-        return newCentroids
+        // TODO: does it make sense to have one tidy inside another?
+        return tf.tidy(() => tf.stack(centroidKTensor).mean(0))
       }))
-    })
-    this.centroids = result;
+    });
   }
 
   /**
-   * Calculate the Euclidian distance between two tensors.
-   * @param {tf.tensor} tensor1 
-   * @param {tf.tensor} tensor2
+   * Calculate the Euclidean distance between two tensors.
+   * @param {tf.Tensor2D} tensor1
+   * @param {tf.Tensor2D} tensor2
+   * @return {number}
    */
-  static getEuclidianDistance(tensor1, tensor2) {
-    // calculate euclidian distance between two arrays
+  static getEuclideanDistance(tensor1, tensor2) {
+    // calculate euclidean distance between two arrays
     const distTensor = tf.tidy(() => {
       const distance = tf.squaredDifference(tensor1, tensor2).sum().sqrt();
       return distance.dataSync()
