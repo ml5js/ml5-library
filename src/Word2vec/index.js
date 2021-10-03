86335322c72b83985d8d46533fca2e4145e7cc24
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-10-03 08:46:01 -0400

    Update multi-line string approach

--- src/Word2vec/index.js
+++ src/Word2vec/index.js
@@ -10,12 +10,12 @@ Word2Vec
 const word2vec = () => {
   console.log('%cword2vec has been disabled', 'color: #A155FF; font-size: 16px');
   console.log(
-    `We've intentionally disabled the word2vec function after recognizing it has the\
-    potential to produce harmful outputs while using the model files included in our\
-    examples. We'll consider reenabling the word2vec function along with changes to\
-    address these issues in a future release of ml5.js. You can find further updates\
-    about this topic on our Twitter account (https://twitter.com/ml5js) and GitHub\
-    (https://github.com/ml5js/ml5-library/issues/1238).`,
+    `We've intentionally disabled the word2vec function after recognizing it has the ` +
+    `potential to produce harmful outputs while using the model files included in our ` +
+    `examples. We'll consider reenabling the word2vec function along with changes to ` +
+    `address these issues in a future release of ml5.js. You can find further updates ` +
+    `about this topic on our Twitter account (https://twitter.com/ml5js) and GitHub ` +
+    `(https://github.com/ml5js/ml5-library/issues/1238).`,
   );
 };
 
