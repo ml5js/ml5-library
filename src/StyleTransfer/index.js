eabda7f79d0b40cb6a5ef64bf32054b04f360bd1
Linda Paiste <lindapaiste@gmail.com>
2022-04-16 16:21:44 -0500

    Fix memory leaks in StyleTransfer.

--- src/StyleTransfer/index.js
+++ src/StyleTransfer/index.js
@@ -30,9 +30,9 @@ const convertCanvasToImage = canvas => {
 class StyleTransfer extends Video {
   /**
    * Create a new Style Transfer Instance。
-   * @param {model} model - The path to Style Transfer model.
+   * @param {string} model - The path to Style Transfer model.
    * @param {HTMLVideoElement || p5.Video} video  - Optional. A HTML video element or a p5 video element.
-   * @param {funciton} callback - Optional. A function to be called once the model is loaded. If no callback is provided, it will return a promise that will be resolved once the model has loaded.
+   * @param {function} callback - Optional. A function to be called once the model is loaded. If no callback is provided, it will return a promise that will be resolved once the model has loaded.
    */
   constructor(model, video, callback) {
     super(video, IMAGE_SIZE);
@@ -42,7 +42,11 @@ class StyleTransfer extends Video {
      * @public
      */
     this.ready = false;
-    this.variableDictionary = {};
+    /**
+     * @private
+     * @type {Record<string, tf.Tensor>}
+     */
+    this.variables = {};
     this.timesScalar = tf.scalar(150);
     this.plusScalar = tf.scalar(255.0 / 2);
     this.epsilonScalar = tf.scalar(1e-3);
@@ -51,6 +55,11 @@ class StyleTransfer extends Video {
     // this.then = this.ready.then;
   }
 
+  /**
+   * @private
+   * @param {string} model
+   * @return {Promise<StyleTransfer>}
+   */
   async load(model) {
     if (this.videoElt) {
       await this.loadVideo();
@@ -59,11 +68,22 @@ class StyleTransfer extends Video {
     return this;
   }
 
+  /**
+   * @private
+   * @param {string} path
+   * @return {Promise<void>}
+   */
   async loadCheckpoints(path) {
     const checkpointLoader = new CheckpointLoader(path);
     this.variables = await checkpointLoader.getAllVariables();
   }
 
+  /**
+   * @private
+   * @param {tf.Tensor} input
+   * @param {number} id
+   * @return {tf.Tensor3D}
+   */
   instanceNorm(input, id) {
     return tf.tidy( () => {
       const [height, width, inDepth] = input.shape;
@@ -79,36 +99,60 @@ class StyleTransfer extends Video {
     });
   }
 
+  /**
+   * @private
+   * @param {tf.Tensor3D} input
+   * @param {number} strides
+   * @param {boolean} relu
+   * @param {number} id
+   * @return {tf.Tensor3D}
+   */
   convLayer(input, strides, relu, id) {
-    const y = tf.conv2d(input, this.variables[StyleTransfer.getVariableName(id)], [strides, strides], 'same');
-    const y2 = this.instanceNorm(y, id + 1);
-    if (relu) {
-      return tf.relu(y2);
-    }
-    return y2;
-  }
-
-  residualBlock(input, id) {
-    const conv1 = this.convLayer(input, 1, true, id);
-    const conv2 = this.convLayer(conv1, 1, false, id + 3);
-    return tf.add(conv2, input);
-  }
-
-  convTransposeLayer(input, numFilters, strides, id) {
-    const [height, width] = input.shape;
-    const newRows = height * strides;
-    const newCols = width * strides;
-    const newShape = [newRows, newCols, numFilters];
-    const y = tf.conv2dTranspose(input, this.variables[StyleTransfer.getVariableName(id)], newShape, [strides, strides], 'same');
-    const y2 = this.instanceNorm(y, id + 1);
-    const y3 = tf.relu(y2);
-    return y3;
+    return tf.tidy(() => {
+      const y = tf.conv2d(input, this.variables[StyleTransfer.getVariableName(id)], [strides, strides], 'same');
+      const y2 = this.instanceNorm(y, id + 1);
+      return relu ? tf.relu(y2) : y2;
+    });
   }
 
   /**
-   * 
-   * @param {Image || p5.Image || HTMLVideoElement || p5.Video} input  - A HTML video or image element or a p5 image or video element. If no input is provided, the default is to use the video element given in the constructor.
-   * @param {funciton} callback - Optional. A function to run once the model has made the transfer. If no callback is provided, it will return a promise that will be resolved once the model has made the transfer.
+   * @private
+   * @param {tf.Tensor3D} input
+   * @param {number} id
+   * @return {tf.Tensor3D}
+   */
+  residualBlock(input, id) {
+    return tf.tidy(() => {
+      const conv1 = this.convLayer(input, 1, true, id);
+      const conv2 = this.convLayer(conv1, 1, false, id + 3);
+      return tf.add(conv2, input);
+    })
+  }
+
+  /**
+   * @param {tf.Tensor3D} input
+   * @param {number} numFilters
+   * @param {number} strides
+   * @param {number} id
+   * @return {tf.Tensor3D}
+   */
+  convTransposeLayer(input, numFilters, strides, id) {
+    return tf.tidy(() => {
+      const [height, width] = input.shape;
+      const newRows = height * strides;
+      const newCols = width * strides;
+      const newShape = [newRows, newCols, numFilters];
+      const y = tf.conv2dTranspose(input, this.variables[StyleTransfer.getVariableName(id)], newShape, [strides, strides], 'same');
+      const y2 = this.instanceNorm(y, id + 1);
+      const y3 = tf.relu(y2);
+      return y3;
+    })
+  }
+
+  /**
+   * @public
+   * @param {Image || p5.Image || HTMLVideoElement || p5.Video || function} inputOrCallback  - A HTML video or image element or a p5 image or video element. If no input is provided, the default is to use the video element given in the constructor.
+   * @param {function} [cb] - Optional. A function to run once the model has made the transfer. If no callback is provided, it will return a promise that will be resolved once the model has made the transfer.
    */
   async transfer(inputOrCallback, cb) {
     let input;
@@ -134,6 +178,11 @@ class StyleTransfer extends Video {
     return callCallback(this.transferInternal(input), callback);
   }
 
+  /**
+   * @private
+   * @param {ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement} input
+   * @return {Promise<HTMLImageElement>}
+   */
   async transferInternal(input) {
     const image = tf.browser.fromPixels(input);
     const result = array3DToImage(tf.tidy(() => {
@@ -158,6 +207,18 @@ class StyleTransfer extends Video {
     image.dispose();
     await tf.nextFrame();
     return result;
+  }
+
+  /**
+   * Dispose of all Tensors in instance properties.
+   * @public
+   * @return {void}
+   */
+  dispose() {
+    Object.values(this.variables).forEach(variable => variable.dispose());
+    this.timesScalar.dispose();
+    this.plusScalar.dispose();
+    this.epsilonScalar.dispose();
   }
 
   // Static Methods
